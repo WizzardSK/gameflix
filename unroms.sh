f23049062fbe318551a37f39070082adb076be14
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-01-08 12:07:00 +0100

    Update unroms.sh

--- unroms.sh
+++ unroms.sh
@@ -1,30 +1,85 @@
 #!/bin/bash
 
 fusermount -u ~/roms/amiga
+fusermount -u ~/.emulationstation/downloaded_media/amiga/screenshots
+
 fusermount -u ~/roms/amstradcpc
+fusermount -u ~/.emulationstation/downloaded_media/amstradcpc/screenshots
+
 fusermount -u ~/roms/arcade
+fusermount -u ~/.emulationstation/downloaded_media/arcade/screenshots
+
 fusermount -u ~/roms/atari2600
+fusermount -u ~/.emulationstation/downloaded_media/atari2600/screenshots
+
 fusermount -u ~/roms/atari5200
+fusermount -u ~/.emulationstation/downloaded_media/atari5200/screenshots
+
 fusermount -u ~/roms/atari7800
+fusermount -u ~/.emulationstation/downloaded_media/atari7800/screenshots
+
 fusermount -u ~/roms/atari800
+fusermount -u ~/.emulationstation/downloaded_media/atari800/screenshots
+
 fusermount -u ~/roms/atarijaguar
+fusermount -u ~/.emulationstation/downloaded_media/atarijaguar/screenshots
+
 fusermount -u ~/roms/atarilynx
+fusermount -u ~/.emulationstation/downloaded_media/atarilynx/screenshots
+
 fusermount -u ~/roms/atarist
+fusermount -u ~/.emulationstation/downloaded_media/atarist/screenshots
+
 fusermount -u ~/roms/c64
+fusermount -u ~/.emulationstation/downloaded_media/c64/screenshots
+
 fusermount -u ~/roms/mame
+fusermount -u ~/.emulationstation/downloaded_media/mame/screenshots
+
 fusermount -u ~/roms/psx
+fusermount -u ~/.emulationstation/downloaded_media/psx/screenshots
+
 fusermount -u ~/roms/ps2
+fusermount -u ~/.emulationstation/downloaded_media/ps2/screenshots
+
 fusermount -u ~/roms/psp
+fusermount -u ~/.emulationstation/downloaded_media/psp/screenshots
+
 fusermount -u ~/roms/sg-1000
+fusermount -u ~/.emulationstation/downloaded_media/sg-1000/screenshots
+
 fusermount -u ~/roms/sega32x
+fusermount -u ~/.emulationstation/downloaded_media/sega32x/screenshots
+
 fusermount -u ~/roms/segacd
-fusermount -u ~/roms/segadc
+fusermount -u ~/.emulationstation/downloaded_media/segacd/screenshots
+
+fusermount -u ~/roms/dreamcast
+fusermount -u ~/.emulationstation/downloaded_media/dreamcast/screenshots
+
 fusermount -u ~/roms/gamegear
+fusermount -u ~/.emulationstation/downloaded_media/gamegear/screenshots
+
 fusermount -u ~/roms/genesis
+fusermount -u ~/.emulationstation/downloaded_media/genesis/screenshots
+
 fusermount -u ~/roms/mastersystem
-fusermount -u ~/roms/segasaturn
+fusermount -u ~/.emulationstation/downloaded_media/mastersystem/screenshots
+
+fusermount -u ~/roms/saturn
+fusermount -u ~/.emulationstation/downloaded_media/saturn/screenshots
+
 fusermount -u ~/roms/dos
-fusermount -u ~/roms/gamecube
+fusermount -u ~/.emulationstation/downloaded_media/dos/screenshots
+
+fusermount -u ~/roms/gc
+fusermount -u ~/.emulationstation/downloaded_media/gc/screenshots
+
 fusermount -u ~/roms/n64
+fusermount -u ~/.emulationstation/downloaded_media/n64/screenshots
+
 fusermount -u ~/roms/snes
+fusermount -u ~/.emulationstation/downloaded_media/snes/screenshots
+
 fusermount -u ~/roms/nes
+fusermount -u ~/.emulationstation/downloaded_media/nes/screenshots
