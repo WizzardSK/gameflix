e08751f4934e1d8a00534299d95b913529167610
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-02-07 20:13:17 +0100

    Create batocera.sh

---
+++ batocera.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+params="--no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon"
+
+./rclone mount "myrient:No-Intro/Atari - 2600" /userdata/roms/atari2600/online $params
+./rclone mount "myrient:No-Intro/Atari - 5200" /userdata/roms/atari5200/online $params
+./rclone mount "myrient:No-Intro/Atari - 7800" /userdata/roms/atari7800/online $params
