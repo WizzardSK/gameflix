ae69839d25870efc7fe48ded80c76a8be070216e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-02-07 20:16:49 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -4,3 +4,4 @@ params="--no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --p
 ./rclone mount "myrient:No-Intro/Atari - 2600" /userdata/roms/atari2600/online $params
 ./rclone mount "myrient:No-Intro/Atari - 5200" /userdata/roms/atari5200/online $params
 ./rclone mount "myrient:No-Intro/Atari - 7800" /userdata/roms/atari7800/online $params
+./rclone mount "myrient:No-Intro/Atari - Jaguar (J64)" /userdata/roms/jaguar/online $params
