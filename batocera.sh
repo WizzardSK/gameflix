c23da4388b624f73fe8631a1126cba60bd143d5a
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-02-07 22:02:16 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 params="--no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon"
 
-sleep 20
+sleep 30
 
 ./rclone mount "myrient:No-Intro/Atari - 2600" /userdata/roms/atari2600/online $params
 ./rclone mount "myrient:No-Intro/Atari - 5200" /userdata/roms/atari5200/online $params
