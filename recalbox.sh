6e90bc0528a21e5364bdb971ae2d98de8e67ef83
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-02-23 16:31:14 +0100

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -44,3 +44,5 @@ do
   mkdir -p /recalbox/share/roms/${rom[0]}/online
   rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
 done
+
+touch /recalbox/share/system/done.txt
