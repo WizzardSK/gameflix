f098a51e0cfca8577963afc2fc90c1ab43a825f0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-03-02 19:51:28 +0100

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -41,6 +41,5 @@ for each in "${roms[@]}"
 do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p /recalbox/share/roms/${rom[0]}/online
-  rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon
-  #--vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty 
+  rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty 
 done
