359da6060e78cb210272b1a2c3c47c46eb9b3055
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-03-10 13:23:21 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl -L https://raw.githubusercontent.com/WizzardSK/gameflix/main/batocera.sh | bash
+
+now=1980
+while (( $now < 2020 )); do
+    sleep 1
+    now=$(date '+%Y')
+done
+
+curl -L -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/batocera.sh | bash
