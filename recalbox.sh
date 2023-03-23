63eb9a9e28812a8cfe5c3d985730b0950661c92c
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-03-23 08:54:40 +0100

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -89,7 +89,7 @@ do
     if [[ ! ${line} =~ .*\.(jpg|png|torrent|xml|sqlite|mp3|ogg) ]]; then 
       line2=${line}
       if [[ ${rom[0]} == "mame" ]]; then
-        line2=$(grep -A1 "name=\"${line%.*}\"" /recalbox/share/bios/mame2003-plus/mame2003-plus.xml | grep -Eo '<description>.*</description>' | sed 's/<\/\?description>//g')
+        line2=$(xmllint --xpath "//game[@name='${line%.*}']/description/text()" /recalbox/share/bios/mame2003-plus/mame2003-plus.xml)
       fi
       echo "<game><path>online${rom[3]}/${line}</path><image>../../thumbs/${rom[2]}/${line2%.*}.png</image></game>" >> /recalbox/share/roms/${rom[0]}/gamelist.xml
     fi
