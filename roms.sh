7649b41532a2e648812ec91f1f8b6d7efecaaea5
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-03-31 21:01:18 +0200

    Update roms.sh

--- roms.sh
+++ roms.sh
@@ -38,6 +38,7 @@ wget -O ~/.emulationstation/es_controller_mappings.cfg https://raw.githubusercon
 wget -O ~/.emulationstation/es_input.xml https://raw.githubusercontent.com/WizzardSK/gameflix/main/.emulationstation/es_input.xml
 
 mkdir -p ~/.emulationstation/media
+rm -rf ~/.emulationstation/downloaded_media
 
 rclone mount "archive:retroarchbios" ~/.config/retroarch/system --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
 rclone mount thumbnails: ~/.emulationstation/media --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
@@ -48,9 +49,7 @@ do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p ~/roms/${rom[0]}
   rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
-  rm ~/.emulationstation/downloaded_media/${rom[0]}/screenshots
   ln -s ~/.emulationstation/media/${rom[2]} ~/.emulationstation/downloaded_media/${rom[0]}/screenshots
-  #rclone mount thumbnails:${rom[2]} ~/.emulationstation/downloaded_media/${rom[0]}/screenshots --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
 done
 
 emulationstation &
