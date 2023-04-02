11eea55c5af58d4227a47d3a215605f089197cd6
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-02 22:28:17 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -46,7 +46,7 @@ IFS=","
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p /userdata/roms/${rom[0]}/online
-  rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf
+  rclone mount "${rom[1]}" /userdata/roms/${rom[0]}/online --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf
   mkdir -p /userdata/roms/${rom[0]}/images
   mount -o bind /userdata/thumbs/${rom[2]} /userdata/roms/${rom[0]}/images
 done
