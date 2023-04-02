83306339eceab0fea3709b01b473a35837f0c811
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-02 22:03:12 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -47,8 +47,8 @@ for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p /userdata/roms/${rom[0]}/online
   rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf
-  rm -rf /userdata/roms/${rom[0]}/images
-  ln -s /userdata/thumbs/${rom[2]} /userdata/roms/${rom[0]}/images
+  mkdir -p /userdata/roms/${rom[0]}/images
+  mount -o bind /userdata/thumbs/${rom[2]} /userdata/roms/${rom[0]}/images
 done
 
 curl http://127.0.0.1:1234/reloadgames
