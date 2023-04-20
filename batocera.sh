c0f4e2ee5439d9d5a30dd4c43a73f619b86ce27a
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-20 23:15:56 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ln -s /usr/bin/fusermount /usr/bin/fusermount3
 curl -s -L https://rclone.org/install.sh | bash
-params="--no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf"
 if [ ! -f /userdata/system/.config/rclone/rclone.conf ]; then wget -O /userdata/system/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/.config/rclone/rclone.conf; fi
 declare -a roms=()
 source <(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)
@@ -11,14 +10,14 @@ chvt 3
 clear
 
 mkdir -p /userdata/thumbs
-rclone mount thumbnails: /userdata/thumbs $params
+rclone mount thumbnails: /userdata/thumbs --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf
 IFS=","
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   echo "Mounting ${rom[0]}"
   mkdir -p /userdata/roms/${rom[0]}/online
   mkdir -p /userdata/roms/${rom[0]}/images
-  rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online $params
+  rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --vfs-cache-mode full --daemon --config=/userdata/system/.config/rclone/rclone.conf
   mount -o bind /userdata/thumbs/${rom[2]} /userdata/roms/${rom[0]}/images
 done
 
