d3c186496d007b1901d0c9e561e09133dbd7bf17
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-21 22:45:49 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -9,7 +9,7 @@ wget -O ~/.emulationstation/es_input.xml https://raw.githubusercontent.com/Wizza
 mkdir -p ~/media
 rm -rf ~/.emulationstation/downloaded_media
 
-rclone mount thumbnails: ~/media/ --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
+rclone mount thumbnails: ~/media --daemon
 
 IFS=","
 for each in "${roms[@]}"; do
