4c168c370d1812a75c509b1b3c6d7edb68cdc0ee
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-22 16:55:32 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -22,7 +22,7 @@ done
 
 fuse-zip ~/roms/atari8bit/Atari\ 8bit\ -\ Games\ -\ \[ATR].zip ~/roms/atari800 -o nonempty
 #fuse-zip ~/roms/spectrum/Sinclair\ ZX\ Spectrum\ -\ Games\ -\ \[Z80].zip ~/roms/zxspectrum -o nonempty
-fuse-zip ~/roms/amstrad/Amstrad\ CPC\ -\ Games\ -\ \[DSK].zip ~/roms/amstradcpc -o nonempty
+#fuse-zip ~/roms/amstrad/Amstrad\ CPC\ -\ Games\ -\ \[DSK].zip ~/roms/amstradcpc -o nonempty
 
 emulationstation &
 rclone sync "archive:retroarchbios" ~/.config/retroarch/system
