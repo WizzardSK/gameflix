d5e3f978f2dc909a26994687b86836e60d985958
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-23 20:32:22 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -7,6 +7,7 @@ wget -O ~/.emulationstation/es_controller_mappings.cfg https://raw.githubusercon
 wget -O ~/.emulationstation/es_input.xml https://raw.githubusercontent.com/WizzardSK/gameflix/main/.emulationstation/es_input.xml
 
 mkdir -p ~/media
+mkdir -p ~/myrient
 rm -rf ~/.emulationstation/downloaded_media
 
 rclone mount thumbnails: ~/media --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
