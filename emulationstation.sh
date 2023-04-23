8cd1f4e5d68f0943a4277e75c29a3acdcd50182b
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-04-23 14:12:34 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -24,7 +24,9 @@ done
 
 mkdir -p ~/roms/atari800
 fuse-zip ~/myrient/TOSEC/Atari/8bit/Games/[ATR]/Atari\ 8bit\ -\ Games\ -\ \[ATR].zip ~/roms/atari800 -o nonempty
-#fuse-zip ~/roms/amstrad/Amstrad\ CPC\ -\ Games\ -\ \[DSK].zip ~/roms/amstradcpc -o nonempty
+mkdir -p ~/roms/amstradcpc
+fuse-zip ~/myrient/TOSEC/Amstrad/CPC/Games/[DSK]/Amstrad\ CPC\ -\ Games\ -\ \[DSK].zip ~/roms/amstradcpc -o nonempty
+
 #fuse-zip ~/roms/spectrum/Sinclair\ ZX\ Spectrum\ -\ Games\ -\ \[Z80].zip ~/roms/zxspectrum -o nonempty
 
 emulationstation &
