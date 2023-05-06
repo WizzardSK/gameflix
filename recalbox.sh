98fd89a9ad563550ffe3a8d8065089be7f5f57b2
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-05-06 09:09:48 +0200

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -58,7 +58,6 @@ for each in "${roms[@]}"; do
     rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
   else
     mount -o bind /recalbox/share/rom/${rom[1]} /recalbox/share/roms/${rom[0]}/online
-    #rclone mount myrient:${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
   fi
   > /recalbox/share/roms/${rom[0]}/gamelist.xml
   echo "<gameList>" >> /recalbox/share/roms/${rom[0]}/gamelist.xml
