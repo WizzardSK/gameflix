a51d69ede91c9007241e862ff631223c3c31af20
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-06-04 19:25:46 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -29,12 +29,13 @@ for each in "${roms[@]}"; do
   ln -s ~/media/${rom[2]}/Named_Snaps ~/.emulationstation/downloaded_media/${rom[0]}/screenshots
 done
 
+IFS=","
 for each in "${zips[@]}"; do
   read -ra zip < <(printf '%s' "$each")
   mkdir -p ~/roms/${zip[0]}
   mkdir -p ~/.emulationstation/downloaded_media/${zip[0]}
   ln -s ~/media/${zip[2]}/Named_Snaps ~/.emulationstation/downloaded_media/${zip[0]}/screenshots
-  fuse-zip ~/myrient/${zip[1]} ~/roms/${zip[O]} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+  fuse-zip ~/myrient/${zip[1]} ~/roms/${zip[O]}/${zip[3]} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
 done
 
 #mkdir -p ~/roms/atari800
