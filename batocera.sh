ace288226ac0422ff8e283a28aee05a9956d4ea3
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-06-08 12:21:40 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -29,4 +29,5 @@ done
 
 chvt 2
 curl http://127.0.0.1:1234/reloadgames
-rclone sync archive:retroarchbios /userdata/bios --config=/userdata/system/.config/rclone/rclone.conf
+
+# rclone sync archive:retroarchbios /userdata/bios --config=/userdata/system/.config/rclone/rclone.conf
