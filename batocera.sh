fb80aa759b14b1e14b8f934fc6eb0de75f64a984
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-06-09 09:58:50 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl https://rclone.org/install.sh | bash
 if [ ! -f /userdata/system/.config/rclone/rclone.conf ]; then wget -O /userdata/system/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/.config/rclone/rclone.conf; fi
 declare -a roms=()
 source <(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)
