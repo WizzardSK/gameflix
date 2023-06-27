0c980c9444230d7717f90c8a992bfb20bdcaf111
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-06-27 17:56:49 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -27,7 +27,7 @@ IFS=","
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p ~/.emulationstation/downloaded_media/${rom[0]}
-  if grep -q "archive:" <<< "${rom[1]}"; then
+  if grep -q ":" <<< "${rom[1]}"; then
     mkdir -p ~/roms/${rom[0]}
     rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
   else
