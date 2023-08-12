67946a567568f29e8984d42998f193441acf5615
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-08-12 19:04:25 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -11,6 +11,10 @@ wget -O ~/.local/share/applications/retroarch.sh.desktop https://raw.githubuserc
 chmod +x ~/retroarch.sh
 xdg-mime default ~/.local/share/applications/retroarch.sh.desktop application/zip
 
+fusermount -u ~/media
+fusermount -u ~/myrient/No-Intro
+fusermount -u ~/myrient/Redump
+fusermount -u ~/myrient/TOSEC
 rm -rf ~/.emulationstation/downloaded_media
 rm -rf ~/.emulationstation/gamelists
 rm -rf ~/.cache/rclone
@@ -51,8 +55,9 @@ for each in "${roms[@]}"; do
   {
     while IFS= read -r line; do
       if [[ ! ${line} =~ (\[BIOS\]|\(Beta|\(Demo\)|\(Aftermarket\)|\(Proto|\(Unl\)|\(Program\)|\(Alt\)) ]]; then
+        thumb=$(echo "$line" | tr '&' '_')
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
+        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${thumb%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
         ((pocet++))
         ((total++))
       fi
@@ -77,8 +82,9 @@ for each in "${zips[@]}"; do
   {
     while IFS= read -r line; do
       if [[ ! ${line} =~ (\[BIOS\]|\(Beta|\(Demo\)|\(Aftermarket\)|\(Proto|\(Unl\)|\(Program\)|\(Alt\)) ]]; then
+        thumb=$(echo "$line" | tr '&' '_')        
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
-        echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
+        echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${thumb%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
         ((pocet++))
         ((total++))
       fi
