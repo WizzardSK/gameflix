02141efc6c7f8f3ca7f5abfa866c5ecd99ad1fc5
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-08-13 17:52:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,77 +5,65 @@ filterInput.focus();
 filterInput.addEventListener('input', function () {
     const filterText = filterInput.value.toLowerCase();
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; }
     }
 });
 showHideBeta.addEventListener('change', function () {
     const filterText = "(beta";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideBeta.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideDemo.addEventListener('change', function () {
     const filterText = "(demo";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideDemo.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideAftermarket.addEventListener('change', function () {
     const filterText = "(aftermarket";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideAftermarket.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideProto.addEventListener('change', function () {
     const filterText = "(proto";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideProto.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideUnl.addEventListener('change', function () {
     const filterText = "(unl";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideUnl.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideProgram.addEventListener('change', function () {
     const filterText = "(program";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideProgram.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHideAlt.addEventListener('change', function () {
     const filterText = "(alt";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHideAlt.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
 });
 showHidePirate.addEventListener('change', function () {
     const filterText = "(pirate";
     for (let i = 0; i < figures.length; i++) {
-        const caption = figures[i].getElementsByTagName('figcaption')[0];
-        const captionText = caption.textContent.toLowerCase();
+        const caption = figures[i].getElementsByTagName('figcaption')[0]; const captionText = caption.textContent.toLowerCase();
         if (captionText.includes(filterText)) { if (showHidePirate.checked) { figures[i].style.display = ''; } else { figures[i].style.display = 'none'; } }
     }
-});
-document.addEventListener('click', function () {
-    filterInput.focus();
 });
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
@@ -89,6 +77,8 @@ document.addEventListener('keydown', function (event) {
         showHideProgram.dispatchEvent(new Event('change'));
         showHideAlt.dispatchEvent(new Event('change'));
         showHidePirate.dispatchEvent(new Event('change'));
+    } else {
+        filterInput.focus();
     }
 });
 showHideBeta.dispatchEvent(new Event('change'));
