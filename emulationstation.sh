6fcae8e85b8daf134b6079722bf61356916440f0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-08-18 18:09:01 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -59,7 +59,7 @@ for each in "${roms[@]}"; do
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
         echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\" onerror=\"this.src='http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
-        echo "<figure><a onclick=\"window.location.href=\'https://${location}/${rom[1]}/${ahref}\'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\" onerror=\"this.src='https://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html
+        echo "<figure><a href=\"#\" onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\" onerror=\"this.src='https://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html
         ((pocet++))
         ((total++))
       fi
@@ -90,7 +90,7 @@ for each in "${zips[@]}"; do
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
         echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"  onerror=\"this.src='http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
-        echo "<figure><a onclick=\"window.location.href=\'https://myrient.erista.me/files/${zip[1]}\'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\" onerror=\"this.src='https://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
+        echo "<figure><a href=\"#\" onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\" onerror=\"this.src='https://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${thumb%.*}.png';this.onerror=''\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
         ((pocet++))
         ((total++))
       fi
