21cbdc6f894a1d011504df486e72618b28fb7c8e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-09-04 18:17:27 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -60,14 +60,8 @@ for each in "${roms[@]}"; do
         ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-
-        rom[2]=$(echo ${rom[2]} | sed 's/ /_/g')
-        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"https://github.com/libretro-thumbnails/${rom[2]}/blob/master/Named_Snaps/${line%.*}.png?raw=true\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
-        echo "<figure><a onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"https://github.com/libretro-thumbnails/${rom[2]}/blob/master/Named_Snaps/${line%.*}.png?raw=true\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html      
-        
-//        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
-//        echo "<figure><a onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html
-        
+        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
+        echo "<figure><a onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html        
         echo ${line} >> ~/${rom[0]}.txt;
         ((pocet++))
         ((total++))
@@ -100,7 +94,7 @@ for each in "${zips[@]}"; do
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
         echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
         echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
-        echo "<figure><a onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img title=\"${line%.*}\" loading=lazy src=\"https://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
+        echo "<figure><a onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
         echo ${line} >> ~/${zip[0]}.txt;        
         ((pocet++))
         ((total++))
