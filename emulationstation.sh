3b3a43c41243a4072ca7eaf33889989dfdb22f77
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-09-05 21:33:19 +0200

    Update emulationstation.sh

--- emulationstation.sh
+++ emulationstation.sh
@@ -30,8 +30,8 @@ rclone mount myrient:Redump ~/myrient/Redump --no-checksum --no-modtime --attr-t
 rclone mount myrient:TOSEC ~/myrient/TOSEC --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
 
 IFS=","
-# echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />" > ~/systems.html
-# echo "<frameset border=0 cols='240, 100%'><frame name='menu' src='systems.html'><frame name='main'></frameset>" > ~/gameflix.html
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />" > ~/systems.html
+echo "<frameset border=0 cols='240, 100%'><frame name='menu' src='systems.html'><frame name='main'></frameset>" > ~/gameflix.html
 for each in "${roms[@]}"; do
   ((platforms++))
   read -ra rom < <(printf '%s' "$each")
@@ -47,31 +47,31 @@ for each in "${roms[@]}"; do
   fi
   ln -s ~/media/${rom[2]}/Named_Snaps ~/.emulationstation/downloaded_media/${rom[0]}/screenshots
   mkdir -p ~/.emulationstation/gamelists/${rom[0]}
-  # > ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-  # > ~/${rom[0]}.html
-  # > ~/${rom[0]}.txt
-  # echo "<gameList>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-  # wget -O ~/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  # wget -O ~/online/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  # pocet=0    
-  # {
-  #   while IFS= read -r line; do
-  #     if [[ ! ${line} =~ \[BIOS\] ]]; then
-  #       ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
-  #       thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")
-  #       echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-  #       echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
-  #       echo "<figure><a onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html        
-  #       echo ${line} >> ~/${rom[0]}.txt;
-  #       ((pocet++))
-  #       ((total++))
-  #     fi
-  #   done
-  # } < <(ls ~/roms/${rom[0]})
-  # echo "</gameList>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
-  # echo "</div><script src=\"script.js\"></script>" >> ~/${rom[0]}.html
-  # echo "</div><script src=\"script.js\"></script>" >> ~/online/${rom[0]}.html
-  # echo "<a href='${rom[0]}.html' target='main'>${rom[3]} ($pocet)</a><br />" >> ~/systems.html
+  > ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
+  > ~/${rom[0]}.html
+  > ~/${rom[0]}.txt
+  echo "<gameList>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
+  wget -O ~/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
+  wget -O ~/online/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
+  pocet=0    
+  {
+    while IFS= read -r line; do
+      if [[ ! ${line} =~ \[BIOS\] ]]; then
+        ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
+        thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")
+        echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
+        echo "<figure><a href=\"roms/${rom[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${rom[0]}.html
+        echo "<figure><a onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${rom[0]}.html        
+        echo ${line} >> ~/${rom[0]}.txt;
+        ((pocet++))
+        ((total++))
+      fi
+    done
+  } < <(ls ~/roms/${rom[0]})
+  echo "</gameList>" >> ~/.emulationstation/gamelists/${rom[0]}/gamelist.xml
+  echo "</div><script src=\"script.js\"></script>" >> ~/${rom[0]}.html
+  echo "</div><script src=\"script.js\"></script>" >> ~/online/${rom[0]}.html
+  echo "<a href='${rom[0]}.html' target='main'>${rom[3]} ($pocet)</a><br />" >> ~/systems.html
 done
 for each in "${zips[@]}"; do
   ((platforms++))
@@ -81,30 +81,30 @@ for each in "${zips[@]}"; do
   ln -s ~/media/${zip[2]}/Named_Snaps ~/.emulationstation/downloaded_media/${zip[0]}/screenshots
   mount-zip ~/myrient/${zip[1]} ~/roms/${zip[O]} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
   mkdir -p ~/.emulationstation/gamelists/${zip[0]}
-  # > ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
-  # > ~/${zip[0]}.html
-  # > ~/${zip[0]}.txt
-  # echo "<gameList>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
-  # wget -O ~/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  # wget -O ~/online/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  # pocet=0
-  # {
-  #   while IFS= read -r line; do
-  #     if [[ ! ${line} =~ \[BIOS\] ]]; then
-  #       thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
-  #       echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
-  #       echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
-  #       echo "<figure><a onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
-  #       echo ${line} >> ~/${zip[0]}.txt;        
-  #       ((pocet++))
-  #       ((total++))
-  #     fi
-  #   done
-  # } < <(ls ~/roms/${zip[0]})
-  # echo "</gameList>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
-  # echo "</div><script src=\"script.js\"></script>" >> ~/${zip[0]}.html
-  # echo "</div><script src=\"script.js\"></script>" >> ~/online/${zip[0]}.html
-  # echo "<a href='${zip[0]}.html' target='main'>${zip[3]} ($pocet)</a><br />" >> ~/systems.html
+  > ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
+  > ~/${zip[0]}.html
+  > ~/${zip[0]}.txt
+  echo "<gameList>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
+  wget -O ~/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
+  wget -O ~/online/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
+  pocet=0
+  {
+    while IFS= read -r line; do
+      if [[ ! ${line} =~ \[BIOS\] ]]; then
+        thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
+        echo "<game><path>./${line}</path></game>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
+        echo "<figure><a href=\"roms/${zip[0]}/${line}\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/${zip[0]}.html
+        echo "<figure><a onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img title=\"${line%.*}\" loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></a></figure>" >> ~/online/${zip[0]}.html
+        echo ${line} >> ~/${zip[0]}.txt;        
+        ((pocet++))
+        ((total++))
+      fi
+    done
+  } < <(ls ~/roms/${zip[0]})
+  echo "</gameList>" >> ~/.emulationstation/gamelists/${zip[0]}/gamelist.xml
+  echo "</div><script src=\"script.js\"></script>" >> ~/${zip[0]}.html
+  echo "</div><script src=\"script.js\"></script>" >> ~/online/${zip[0]}.html
+  echo "<a href='${zip[0]}.html' target='main'>${zip[3]} ($pocet)</a><br />" >> ~/systems.html
 done
 echo "<p><b>Total: $total</b>" >> ~/systems.html
 echo "<p><b>Platforms: $platforms</b>" >> ~/systems.html
