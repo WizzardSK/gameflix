deb25367a071383352520e8b01be3b9f4558b2f4
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-09-20 20:01:46 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   mkdir -p ~/.emulationstation/downloaded_media/${rom[0]}
   if grep -q ":" <<< "${rom[1]}"; then
-    location="archive.org/download"
+    location="the-eye.eu/public"
     mkdir -p ~/roms/${rom[0]}
     rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
     rom[1]="${rom[1]/archive:/}"
