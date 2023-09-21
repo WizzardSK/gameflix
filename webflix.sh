c95cdb24eaff9dbcd0e7859ffea7c665e4ac6b06
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-09-21 18:53:11 +0200

    Create webflix.sh

---
+++ webflix.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)
+if [ ! -f ~/.config/rclone/rclone.conf ]; then wget -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/.config/rclone/rclone.conf; fi
+wget -O ~/retroarch.sh https://raw.githubusercontent.com/WizzardSK/gameflix/main/retroarch.sh
+wget -O ~/style.css https://raw.githubusercontent.com/WizzardSK/gameflix/main/style.css
+wget -O ~/script.js https://raw.githubusercontent.com/WizzardSK/gameflix/main/script.js
+wget -O ~/.local/share/applications/retroarch.sh.desktop https://raw.githubusercontent.com/WizzardSK/gameflix/main/retroarch.sh.desktop
+chmod +x ~/retroarch.sh
+xdg-mime default ~/.local/share/applications/retroarch.sh.desktop application/zip
+
+rm -rf ~/.cache/rclone
+rm -rf ~/roms
+mkdir -p ~/media
+mkdir -p ~/myrient
+mkdir -p ~/myrient/No-Intro
+mkdir -p ~/myrient/Redump
+mkdir -p ~/myrient/TOSEC
+mkdir -p ~/roms
+mkdir -p ~/iso
+
+rclone mount thumbnails: ~/media --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
+rclone mount myrient:No-Intro ~/myrient/No-Intro --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
+rclone mount myrient:Redump ~/myrient/Redump --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty --daemon
+rclone mount myrient:TOSEC ~/myrient/TOSEC --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
+
+IFS=","
+for each in "${roms[@]}"; do
+  ((platforms++))
+  read -ra rom < <(printf '%s' "$each")
+  if grep -q ":" <<< "${rom[1]}"; then
+    mkdir -p ~/roms/${rom[0]}
+    rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty --daemon 
+  else
+    ln -s ~/myrient/${rom[1]} ~/roms/${rom[0]}
+  fi
+done
+for each in "${zips[@]}"; do
+  ((platforms++))
+  read -ra zip < <(printf '%s' "$each")
+  mkdir -p ~/roms/${zip[0]}
+  mount-zip ~/myrient/${zip[1]} ~/roms/${zip[O]} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+done
