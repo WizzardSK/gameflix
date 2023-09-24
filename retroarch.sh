466d247328fbb105a5021e0641df42ab28f69508
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-09-24 13:21:38 +0200

    Update retroarch.sh

--- retroarch.sh
+++ retroarch.sh
@@ -41,6 +41,7 @@ case "$adresar" in
 
   "pcengine"|"NEC - PC Engine - TurboGrafx-16")                 core="mednafen_pce_fast" ;;
   "pcenginecd"|"NEC - PC Engine CD & TurboGrafx CD")            command="umount ~/iso; mount-zip \"$1\" ~/iso; /usr/bin/retroarch -L ~/.config/retroarch/cores/mednafen_pce_fast_libretro.so ~/iso/*.cue" ;;
+  "supergrafx"|"NEC - PC Engine SuperGrafx")                    core="mednafen_supergrafx" ;;  
   "3do"|"Panasonic - 3DO Interactive Multiplayer")              command="umount ~/iso; mount-zip \"$1\" ~/iso; /usr/bin/retroarch -L ~/.config/retroarch/cores/opera_libretro.so ~/iso/*.cue" ;;
   "ngp"|"SNK - NeoGeo Pocket")                                  core="mednafen_ngp" ;;
   "ngpc"|"SNK - NeoGeo Pocket Color")                           core="mednafen_ngp" ;;
