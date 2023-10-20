350d332fa16d5b9fc25400f5f945e62ba1b53c67
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-10-20 18:21:37 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -8,18 +8,12 @@ for each in "${roms[@]}"; do
   ((platforms++))
   read -ra rom < <(printf '%s' "$each")
   if grep -q ":" <<< "${rom[1]}"; then
-    location="the-eye.eu/public"
     mkdir -p ~/roms/${rom[0]}
     rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
-    rom[1]="${rom[1]/archive:/}"
-  else
-    location="myrient.erista.me/files"
-    #ln -s ~/myrient/${rom[1]} ~/roms/${rom[0]}
   fi
   > ~/${rom[0]}.html
   #> ~/${rom[0]}.txt
   wget -O ~/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  #wget -O ~/online/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
   pocet=0    
   if [ "${rom[0]}" = "dos" ]; then rom[1]="../roms/dos"; fi
   {
@@ -28,7 +22,6 @@ for each in "${roms[@]}"; do
         ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")
         echo "<figure onclick=\"window.location.href='myrient/${rom[1]}/${ahref}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/${rom[0]}.html
-        #echo "<figure onclick=\"window.location.href='https://${location}/${rom[1]}/${ahref}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/online/${rom[0]}.html        
         #echo ${line} >> ~/${rom[0]}.txt;
         ((pocet++))
         ((total++))
@@ -36,7 +29,6 @@ for each in "${roms[@]}"; do
     done
   } < <(ls ~/myrient/${rom[1]})
   echo "</div><script src=\"script.js\"></script>" >> ~/${rom[0]}.html
-  #echo "</div><script src=\"script.js\"></script>" >> ~/online/${rom[0]}.html
   echo "<a href='${rom[0]}.html' target='main' onclick=\"document.getElementById('platforma').innerHTML = this.innerText\">${rom[3]}</a> ($pocet)<br />" >> ~/systems.html
   ext=""
   if [ -n "${rom[5]}" ]; then
@@ -52,7 +44,6 @@ for each in "${zips[@]}"; do
   > ~/${zip[0]}.html
   #> ~/${zip[0]}.txt
   wget -O ~/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
-  #wget -O ~/online/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
   pocet=0
   {
     while IFS= read -r line; do
@@ -60,7 +51,6 @@ for each in "${zips[@]}"; do
         ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
         echo "<figure onclick=\"window.location.href='roms/${zip[0]}/${ahref}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/${zip[0]}.html
-        #echo "<figure onclick=\"window.location.href='https://myrient.erista.me/files/${zip[1]}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/online/${zip[0]}.html
         #echo ${line} >> ~/${zip[0]}.txt;        
         ((pocet++))
         ((total++))
@@ -68,7 +58,6 @@ for each in "${zips[@]}"; do
     done
   } < <(ls ~/roms/${zip[0]})
   echo "</div><script src=\"script.js\"></script>" >> ~/${zip[0]}.html
-  #echo "</div><script src=\"script.js\"></script>" >> ~/online/${zip[0]}.html
   echo "<a href='${zip[0]}.html' target='main' target='main' onclick=\"document.getElementById('platforma').innerHTML = this.innerText\">${zip[3]}</a> ($pocet)<br />" >> ~/systems.html
   ext=""
   if [ -n "${zip[5]}" ]; then
