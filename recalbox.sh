684f1ec4a3cef26069090e074e144c7134f66683
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-10-20 22:10:35 +0200

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -39,7 +39,7 @@ rclone mount myrient:No-Intro /recalbox/share/rom/No-Intro --config=/recalbox/sh
 rclone mount myrient:Redump /recalbox/share/rom/Redump --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
 rclone mount myrient:TOSEC /recalbox/share/rom/TOSEC --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
 
-IFS=","
+IFS=";"
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   echo "Mounting ${rom[0]}"
