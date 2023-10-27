a2eb721bc2c9bc9a9ba67be8527d5abe36f81642
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-10-27 13:46:10 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -4,7 +4,7 @@ wget -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK
 wget -O ~/style.css https://raw.githubusercontent.com/WizzardSK/gameflix/main/style.css
 wget -O ~/script.js https://raw.githubusercontent.com/WizzardSK/gameflix/main/script.js
 wget -O ~/.local/share/applications/retroarch.sh.desktop https://raw.githubusercontent.com/WizzardSK/gameflix/main/retroarch.sh.desktop
-chmod +x ~/retroarch.sh
+#chmod +x ~/retroarch.sh
 xdg-mime default ~/.local/share/applications/retroarch.sh.desktop application/zip
 
 mkdir -p ~/myrient
