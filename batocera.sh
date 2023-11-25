34372529965c889d60114a4be9a352b90189ea2f
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-11-25 20:45:21 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -10,20 +10,16 @@ mkdir -p /userdata/roms
 mkdir -p /userdata/thumbs
 mkdir -p /userdata/rom
 mkdir -p /userdata/zip
-mkdir -p /userdata/rom/No-Intro
-mkdir -p /userdata/rom/Redump
 
 echo "Mounting thumbs"
 rclone mount thumbnails: /userdata/thumbs --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
-echo "Mounting No-Intro"
-rclone mount myrient:No-Intro /userdata/rom/No-Intro --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
-echo "Mounting Redump"
-rclone mount myrient:Redump /userdata/rom/Redump --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
+echo "Mounting roms"
+rclone mount myrient: /userdata/rom --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
 
 IFS=";"
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
-  echo "Mounting ${rom[0]}"
+  echo "Setting ${rom[0]}"
   mkdir -p /userdata/roms/${rom[0]}/online
   mkdir -p /userdata/roms/${rom[0]}/images  
   if grep -q ":" <<< "${rom[1]}"; then
@@ -33,7 +29,7 @@ for each in "${roms[@]}"; do
 done
 for each in "${zips[@]}"; do
   read -ra zip < <(printf '%s' "$each")
-  echo "Mounting ${zip[0]}"
+  echo "Setting ${zip[0]}"
   mkdir -p /userdata/roms/${zip[0]}/online
   mkdir -p /userdata/roms/${zip[0]}/images
   if [ ! -f /userdata/zip/${zip[0]}.zip ]; then wget -O /userdata/zip/${zip[0]}.zip https://myrient.erista.me/files/${zip[1]}; fi  
