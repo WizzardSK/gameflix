77cbb4817a9f4bc774ef1b821abff149a40353b4
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-11-26 19:20:59 +0100

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -41,7 +41,7 @@ for each in "${roms[@]}"; do
   echo "Mounting ${rom[0]}"
   mkdir -p /recalbox/share/roms/${rom[0]}/online  
   if grep -q ":" <<< "${rom[1]}"; then
-    rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/.config/rclone/rclone.conf --daemon --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
+    rclone mount ${rom[1]} /recalbox/share/roms/${rom[0]}/online --config=/recalbox/share/system/rclone.conf --daemon --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty
   else mount -o bind /recalbox/share/rom/${rom[1]} /recalbox/share/roms/${rom[0]}/online; fi
   > /recalbox/share/roms/${rom[0]}/gamelist.xml
   echo "<gameList>" >> /recalbox/share/roms/${rom[0]}/gamelist.xml
