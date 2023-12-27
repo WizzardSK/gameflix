5e3491c721855aa7d89d83ea21570e4590d42a22
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2023-12-27 16:11:17 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -13,7 +13,6 @@ for each in "${roms[@]}"; do
     rclone mount ${rom[1]} ~/roms/${rom[0]} --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --vfs-cache-mode full --allow-non-empty --daemon
   fi
   > ~/${rom[0]}.html
-  #> ~/${rom[0]}.txt
   wget -O ~/${rom[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
   pocet=0    
   if [ "${rom[0]}" = "dos" ]; then rom[1]="../roms/dos"; fi
@@ -23,7 +22,6 @@ for each in "${roms[@]}"; do
         ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g" -e 's/#/%23/g')
         echo "<figure onclick=\"window.location.href='myrient/${rom[1]}/${ahref}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/${rom[0]}.html
-        #echo ${line} >> ~/${rom[0]}.txt;
         ((pocet++))
         ((total++))
       fi
@@ -31,7 +29,7 @@ for each in "${roms[@]}"; do
   } < <(ls ~/myrient/${rom[1]})
   echo "</div><script src=\"script.js\"></script>" >> ~/${rom[0]}.html
   echo "<a href='${rom[0]}.html' target='main' onclick=\"document.getElementById('platforma').innerHTML = this.innerText\">${rom[3]}</a> ($pocet)<br />" >> ~/systems.html
-  echo "<figure><img src='https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/monochrome/png/"${rom[2]}".png'><figcaption><a href='${rom[0]}.html' target='main'>${rom[3]}</a> ($pocet)</figcaption></figure>" >> ~/main.html
+  echo "<figure><a href='${rom[0]}.html'><img src='https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/systematic/png/"${rom[2]}".png'><figcaption>${rom[2]} ($pocet)</figcaption></a></figure>" >> ~/main.html
   ext=""
   if [ -n "${rom[5]}" ]; then ext="; ext=\"${rom[5]}\""; fi
   echo "\"${rom[1]##*/}\") core=\"${rom[4]}\"${ext};;" >> ~/retroarch.sh
@@ -42,7 +40,6 @@ for each in "${zips[@]}"; do
   mkdir -p ~/roms/${zip[0]}
   mount-zip ~/myrient/${zip[1]} ~/roms/${zip[O]} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
   > ~/${zip[0]}.html
-  #> ~/${zip[0]}.txt
   wget -O ~/${zip[0]}.html https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform.html
   pocet=0
   {
@@ -51,7 +48,6 @@ for each in "${zips[@]}"; do
         ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g")    
         echo "<figure onclick=\"window.location.href='roms/${zip[0]}/${ahref}'\"><img loading=lazy src=\"http://thumbnails.libretro.com/${zip[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure>" >> ~/${zip[0]}.html
-        #echo ${line} >> ~/${zip[0]}.txt;        
         ((pocet++))
         ((total++))
       fi
@@ -59,7 +55,7 @@ for each in "${zips[@]}"; do
   } < <(ls ~/roms/${zip[0]})
   echo "</div><script src=\"script.js\"></script>" >> ~/${zip[0]}.html
   echo "<a href='${zip[0]}.html' target='main' onclick=\"document.getElementById('platforma').innerHTML = this.innerText\">${zip[3]}</a> ($pocet)<br />" >> ~/systems.html  
-  echo "<figure><img src='https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/monochrome/png/"${zip[2]}".png'><figcaption><a href='${zip[0]}.html' target='main'>${zip[3]}</a> ($pocet)</figcaption></figure>" >> ~/main.html  
+  echo "<figure><a href='${zip[0]}.html'><img src='https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/systematic/png/"${zip[2]}".png'><figcaption>${zip[2]} ($pocet)</figcaption></a></figure>" >> ~/main.html  
   ext=""
   if [ -n "${zip[5]}" ]; then ext="; ext=\"${zip[5]}\""; fi
   echo "\"${zip[0]}\") core=\"${zip[4]}\"${ext};;" >> ~/retroarch.sh
