97fe9a939ab68bf650251e6906364e10976ca4d6
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-01-08 21:10:16 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -13,8 +13,8 @@ mkdir -p /userdata/thumbs
 mkdir -p /userdata/rom
 mkdir -p /userdata/zip
 
-echo "Mounting thumbs"
-rclone mount thumbnails: /userdata/thumbs --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
+#echo "Mounting thumbs"
+#rclone mount thumbnails: /userdata/thumbs --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
 echo "Mounting roms"
 rclone mount myrient: /userdata/rom --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
 
@@ -27,8 +27,8 @@ for each in "${roms[@]}"; do
   if grep -q ":" <<< "${rom[1]}"; then
     rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
   else mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/online; fi
-  mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
-  #rm -f /userdata/roms/${rom[0]}/online/\[BIOS\]*.*
+  #mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
+  rclone sync "thumbnails:${rom[2]}/Named_Snaps" /userdata/roms/${rom[0]}/images
 done
 for each in "${zips[@]}"; do
   read -ra zip < <(printf '%s' "$each")
@@ -37,7 +37,8 @@ for each in "${zips[@]}"; do
   mkdir -p /userdata/roms/${zip[0]}/images
   if [ ! -f /userdata/zip/${zip[0]}.zip ]; then wget -O /userdata/zip/${zip[0]}.zip https://myrient.erista.me/files/${zip[1]}; fi  
   /userdata/system/mount-zip /userdata/zip/${zip[0]}.zip /userdata/roms/${zip[O]}/online -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
-  mount -o bind /userdata/thumbs/${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images
+  #mount -o bind /userdata/thumbs/${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images
+  rclone sync "thumbnails:${zip[2]}/Named_Snaps" /userdata/roms/${zip[0]}/images
 done
 
 wget -O /usr/share/emulationstation/es_systems.cfg https://github.com/WizzardSK/gameflix/raw/main/batocera/share/system/es_systems.cfg
