d26caff978f5bd96e6cc8afd104d6838fe275691
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-01-17 21:18:06 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -7,14 +7,14 @@ source <(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platf
 
 emulationstation stop; chvt 3; clear
 
-#rm -rf /userdata/roms
+rm -rf /userdata/roms
 mkdir -p /userdata/roms
 mkdir -p /userdata/thumbs
 mkdir -p /userdata/rom
 mkdir -p /userdata/zip
 
 #echo "Mounting thumbs"
-#rclone mount thumbnails: /userdata/thumbs --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
+rclone mount thumbnails: /userdata/thumbs --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
 echo "Mounting roms"
 rclone mount myrient: /userdata/rom --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
 
@@ -27,7 +27,7 @@ for each in "${roms[@]}"; do
   if grep -q ":" <<< "${rom[1]}"; then
     rclone mount ${rom[1]} /userdata/roms/${rom[0]}/online --http-no-head --no-checksum --no-modtime --dir-cache-time 100h --allow-non-empty --attr-timeout 100h --poll-interval 100h --daemon --config=/userdata/system/rclone.conf
   else mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/online; fi
-  #mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
+  mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
   #rclone sync thumbnails:${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images --ignore-existing --config=/userdata/system/rclone.conf
 done
 for each in "${zips[@]}"; do
@@ -37,7 +37,7 @@ for each in "${zips[@]}"; do
   mkdir -p /userdata/roms/${zip[0]}/images
   if [ ! -f /userdata/zip/${zip[0]}.zip ]; then wget -O /userdata/zip/${zip[0]}.zip https://myrient.erista.me/files/${zip[1]}; fi  
   /userdata/system/mount-zip /userdata/zip/${zip[0]}.zip /userdata/roms/${zip[O]}/online -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
-  #mount -o bind /userdata/thumbs/${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images
+  mount -o bind /userdata/thumbs/${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images
   #rclone sync thumbnails:${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images --ignore-existing --config=/userdata/system/rclone.conf
 done
 
