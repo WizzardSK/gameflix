c2f154bcc0e0638b305e6c51cdcfee580445cf66
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-02-18 10:18:37 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -72,7 +72,7 @@ for each in "${isos[@]}"; do
       fi
     done
   } < <(ls ~/myrient/${rom[1]})
-  echo "</div><script src=\"script.js\"></script>" >> ~/gameflix/${rom[0]}.html
+  echo "</div><script src=\"script.js\"></script>" >> ~/gameflix/${rom[0]}-iso.html
   echo "<a href='${rom[0]}-iso.html' target='main'>${rom[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html
   echo "<figure><a href='${rom[0]}-iso.html'><img src='https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/monochrome/png/"${rom[2]}".png'><figcaption>${rom[2]} ($pocet)</figcaption></a></figure>" >> ~/gameflix/main.html
   ext=""
