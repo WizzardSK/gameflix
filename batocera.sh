f937b902067a307cbdf160f4a5a6406b6a097c7d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-02-28 15:18:18 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -s /usr/bin/fusermount /usr/bin/fusermount3
 curl https://rclone.org/install.sh | bash
-wget -O /userdata/system/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/.config/rclone/rclone.conf
+wget -O /userdata/system/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 if [ ! -f /userdata/system/mount-zip ]; then wget -O /userdata/system/mount-zip https://github.com/WizzardSK/gameflix/raw/main/batocera/share/system/mount-zip; chmod +x /userdata/system/mount-zip; fi
 source <(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)
 
