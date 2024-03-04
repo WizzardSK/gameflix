ba9c7622ab4cd4fd298773551d481ea6db9412a2
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-03-04 17:29:55 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mount -o remount,size=6000M /tmp
 ln -s /usr/bin/fusermount /usr/bin/fusermount3
 curl https://rclone.org/install.sh | bash
 wget -O /userdata/system/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
