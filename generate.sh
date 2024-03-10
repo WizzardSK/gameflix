e78b1c33a97756bd2331e7d0721c81e6b7405373
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-03-10 12:11:28 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ for each in "${roms[@]}"; do
   if [ "${rom[0]}" = "dos" ]; then rom[1]="../roms/dos-other"; fi
   if [ -e ~/gameflix/${rom[0]}.html ]; then
     pocet=$(ls ~/myrient/${rom[1]} -1 | wc -l)
-    total=$(($pocet+$total))
+    total=$((pocet+total))
     echo "<a href='${rom[0]}.html' target='main'>${rom[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html
     continue
   fi
@@ -46,7 +46,7 @@ for each in "${romz[@]}"; do
   read -ra zip < <(printf '%s' "$each")
   if [ -e ~/gameflix/${zip[0]}-rom.html ]; then
     pocet=$(ls ~/roms/${zip[0]}-zip -1 | wc -l)
-    total=$(($pocet+$total))
+    total=$((pocet+total))
     echo "<a href='${zip[0]}-rom.html' target='main'>${zip[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html  
     continue
   fi  
@@ -78,7 +78,7 @@ for each in "${zips[@]}"; do
   read -ra zip < <(printf '%s' "$each")
   if [ -e ~/gameflix/${zip[0]}-zip.html ]; then
     pocet=$(ls ~/roms/${zip[0]} -1 | wc -l)
-    total=$(($pocet+$total))
+    total=$((pocet+total))
     echo "<a href='${zip[0]}-zip.html' target='main'>${zip[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html  
     continue
   fi
@@ -110,7 +110,7 @@ for each in "${isos[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   if [ -e ~/gameflix/${rom[0]}-iso.html ]; then
     pocet=$(ls ~/myrient/${rom[1]} -1 | wc -l)
-    total=$(($pocet+$total))
+    total=$((pocet+total))
     echo "<a href='${rom[0]}-iso.html' target='main'>${rom[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html
     continue
   fi
