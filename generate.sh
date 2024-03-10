85f8289767c5c6a92a54c45a62b61eaf0f74a598
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-03-10 12:02:50 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ echo "<title>gameflix</title><frameset border=0 cols='240, 100%'><frame name='me
 wget -O ~/gameflix/retroarch.sh https://raw.githubusercontent.com/WizzardSK/gameflix/main/retroarch.1st
 wget -O ~/gameflix/style.css https://raw.githubusercontent.com/WizzardSK/gameflix/main/style.css
 wget -O ~/gameflix/script.js https://raw.githubusercontent.com/WizzardSK/gameflix/main/script.js
+total=0
 
 for each in "${roms[@]}"; do
   ((platforms++))
