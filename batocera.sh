41f2d8d01dac09dbf354b68f3cdea45fe2971069
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-04-02 16:05:18 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -10,7 +10,9 @@ emulationstation stop; chvt 3; clear
 
 mkdir -p /userdata/rom
 mkdir -p /userdata/roms
+mkdir -p /userdata/thumb
 mkdir -p /userdata/thumbs
+mkdir -p /userdata/snaps
 mkdir -p /userdata/zip
 mkdir -p /userdata/romz
 
@@ -28,7 +30,11 @@ for each in "${roms[@]}"; do
   if grep -q ":" <<< "${rom[1]}"; then
     rclone mount ${rom[1]} /userdata/roms/${rom[0]}/Redump-online --http-no-head --no-checksum --no-modtime --dir-cache-time 1000h --allow-non-empty --attr-timeout 1000h --poll-interval 1000h --daemon --config=/userdata/system/rclone.conf
   else mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/Redump-online; fi
-  mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
+  #mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
+  if [ ! -f /userdata/thumb/${rom[0]}.zip ]; then wget -O /userdata/thumb/${rom[0]}.zip https://github.com/WizzardSK/${rom[2]// /_}/archive/refs/heads/master.zip; fi
+  mkdir -p /userdata/snaps/${rom[0]}
+  /userdata/system/mount-zip /userdata/thumb/${rom[0]}.zip /userdata/snaps/${rom[0]}
+  mount -o bind /userdata/snaps/${rom[0]}/${rom[2]// /_}-master/Named_Snaps /userdata/roms/${rom[0]}/images
 done
 for each in "${isos[@]}"; do
   read -ra iso < <(printf '%s' "$each")
@@ -43,7 +49,7 @@ for each in "${romz[@]}"; do
   echo "n-i: ${zip[2]}"
   mkdir -p /userdata/roms/${zip[0]}/No-Intro
   mkdir -p /userdata/roms/${zip[0]}/images
-  if [ ! -f /userdata/romz/${zip[0]}.zip ]; then wget -O /userdata/romz/${zip[0]}.zip https://archive.org/download/ni-roms/roms/${zip[1]}; fi  
+  if [ ! -f /userdata/romz/${zip[0]}.zip ]; then wget -O /userdata/romz/${zip[0]}.zip https://archive.org/download/ni-roms/roms/${zip[1]}; fi
   /userdata/system/mount-zip /userdata/romz/${zip[0]}.zip /userdata/roms/${zip[O]}/No-Intro -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
   mount -o bind /userdata/thumbs/${zip[2]}/Named_Snaps /userdata/roms/${zip[0]}/images
 done
