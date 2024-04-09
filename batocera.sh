a5481f5c98b3b1fb67b502651f43a3fbc653bc02
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-04-09 23:31:51 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -25,7 +25,7 @@ IFS=";"
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   echo "rom: ${rom[2]}"
-  rom[3]=${rom[3]//\/};
+  rom[3]=${rom[3]//\/}
   mkdir -p /userdata/roms/${rom[0]}/${rom[3]//<[^>]*>/}
   mkdir -p /userdata/roms/${rom[0]}/images  
   if grep -q ":" <<< "${rom[1]}"; then
