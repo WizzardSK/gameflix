dcd9f842224483c920c8003a18cf3d425778b77b
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-04-14 18:09:09 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -19,18 +19,31 @@ for each in "${roms[@]}"; do
     mkdir -p ~/roms/${rom[0]}-other
     rclone mount ${rom[1]} ~/roms/${rom[0]}-other --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate 
   fi
+
+  rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
+  if [[ $filename =~ \.zip$ ]]; then
+    mkdir -p ~/roms/${rom3}
+    if [ ! -f ~/zip/${rom3}.zip ]; then 
+      if [[ "${rom[1]}" == *://* ]]; then
+        wget -O ~/zip/${rom3}.zip ${rom[1]};
+      else
+        wget -O ~/zip/${rom3}.zip https://myrient.erista.me/files/${rom[1]};
+      fi
+    fi  
+    mount-zip ~/zip/${rom3}.zip ~/roms/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+  fi
 done
 
-for each in "${zips[@]}"; do
-  read -ra zip < <(printf '%s' "$each")
-  rom3=$(sed 's/<[^>]*>//g' <<< "${zip[3]}")
-  mkdir -p ~/roms/${rom3}
-  if [ ! -f ~/zip/${rom3}.zip ]; then 
-    if [[ "${zip[1]}" == *://* ]]; then
-      wget -O ~/zip/${rom3}.zip ${zip[1]};
-    else
-      wget -O ~/zip/${rom3}.zip https://myrient.erista.me/files/${zip[1]};
-    fi
-  fi  
-  mount-zip ~/zip/${rom3}.zip ~/roms/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
-done
+#for each in "${zips[@]}"; do
+#  read -ra zip < <(printf '%s' "$each")
+#  rom3=$(sed 's/<[^>]*>//g' <<< "${zip[3]}")
+#  mkdir -p ~/roms/${rom3}
+#  if [ ! -f ~/zip/${rom3}.zip ]; then 
+#    if [[ "${zip[1]}" == *://* ]]; then
+#      wget -O ~/zip/${rom3}.zip ${zip[1]};
+#    else
+#      wget -O ~/zip/${rom3}.zip https://myrient.erista.me/files/${zip[1]};
+#    fi
+#  fi  
+#  mount-zip ~/zip/${rom3}.zip ~/roms/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+#done
