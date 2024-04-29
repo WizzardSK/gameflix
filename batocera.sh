f1d3d02356c0d7a63e468a26eb96ed5a9056b5d7
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-04-29 12:30:20 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -30,8 +30,9 @@ for each in "${roms[@]}"; do
   mkdir -p /userdata/roms/${rom[0]}/${rom3}
   mkdir -p /userdata/roms/${rom[0]}/images  
   if [[ ${rom[1]} =~ \.zip$ ]]; then
-    if [ ! -f /userdata/zip/${rom3}.zip ]; then wget -O /userdata/zip/${rom3}.zip https://myrient.erista.me/files/${rom[1]}; fi  
-    /userdata/system/mount-zip /userdata/zip/${rom3}.zip /userdata/roms/${rom[O]}/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+    #if [ ! -f /userdata/zip/${rom3}.zip ]; then wget -O /userdata/zip/${rom3}.zip https://myrient.erista.me/files/${rom[1]}; fi  
+    #/userdata/system/mount-zip /userdata/zip/${rom3}.zip /userdata/roms/${rom[O]}/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
+    /userdata/system/ratarmount /userdata/rom/${rom[1]} /userdata/roms/${rom3} -o nonempty -omodules=iconv,from_code=$charset1,to_code=$charset2
   else
     if grep -q ":" <<< "${rom[1]}"; then
       rclone mount ${rom[1]} /userdata/roms/${rom[0]}/${rom3} --http-no-head --no-checksum --no-modtime --dir-cache-time 1000h --allow-non-empty --attr-timeout 1000h --poll-interval 1000h --daemon --config=/userdata/system/rclone.conf
