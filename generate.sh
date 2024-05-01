535470552d9c35566a588c1012b552a2e110ea6a
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-05-01 19:47:35 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -40,7 +40,6 @@ for each in "${roms[@]}"; do
   {
     while IFS= read -r line; do
       if [[ ! ${line} =~ \[BIOS\] ]]; then
-        ahref=$(echo "$line" | sed -e "s/'/\\\'/g")
         thumb=$(echo "$line" | sed -e 's/&/_/g' -e "s/'/\\\'/g" -e 's/#/%23/g')
         echo "<a href=\"../$romfolder/${ahref}\" target=main><figure><img loading=lazy src=\"http://thumbnails.libretro.com/${rom[2]}/Named_Snaps/${line%.*}.png\"><figcaption>${line%.*}</figcaption></figure></a>" >> ~/gameflix/${rom3}.html
         ((pocet++))
