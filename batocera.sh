32fd5d939a6aff0e7850feb003f923e6e2993a4b
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-05-28 20:28:03 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -19,7 +19,8 @@ mkdir -p /userdata/system/.cache/ratarmount
 echo "Mounting thumbs"
 /userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs http://thumbnails.libretro.com /userdata/thumbs
 echo "Mounting roms"
-/userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs https://myrient.erista.me/files /userdata/rom
+#/userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs https://myrient.erista.me/files /userdata/rom
+rclone mount myrient: /userdata/rom --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate
 
 IFS=";"
 for each in "${roms[@]}"; do
