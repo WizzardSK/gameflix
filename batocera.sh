808022200b91acff32541eb7abd99e64d773eeee
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-05 21:41:20 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -15,9 +15,11 @@ mkdir -p /userdata/roms
 mkdir -p /userdata/thumbs
 mkdir -p /userdata/system/.cache/httpdirfs
 mkdir -p /userdata/system/.cache/ratarmount
+mkdir -p /userdata/system/.cache/rclone
 
 echo "Mounting thumbs"
-/userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs http://thumbnails.libretro.com /userdata/thumbs
+#/userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs http://thumbnails.libretro.com /userdata/thumbs
+rclone mount thumbnails: /userdata/thumbs --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf --cache-dir=/userdata/system/.cache/rclone
 echo "Mounting roms"
 #/userdata/system/httpdirfs --cache --no-range-check --cache-location /userdata/system/.cache/httpdirfs https://myrient.erista.me/files /userdata/rom
 rclone mount myrient: /userdata/rom --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf
