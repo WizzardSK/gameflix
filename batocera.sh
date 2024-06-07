806b9433b077b8edaf6123f814b729b560e1cddb
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-07 19:22:26 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -42,7 +42,7 @@ for each in "${roms[@]}"; do
     fi
   fi  
   #mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
-  rclone mount thumbnails:${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf
+  rclone mount thumbnails:${rom[2]}/Named_Snaps/ /userdata/roms/${rom[0]}/images --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf
 done
 
 wget -O /usr/share/emulationstation/es_systems.cfg https://github.com/WizzardSK/gameflix/raw/main/batocera/share/system/es_systems.cfg
