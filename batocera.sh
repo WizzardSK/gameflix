0d023f0ce8e6f8411d32541c8620634a8dcc9778
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-11 22:07:44 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -47,7 +47,7 @@ for each in "${roms[@]}"; do
   fi  
   if ! findmnt -rn /userdata/roms/${rom[0]}/images > /dev/null; then
     echo "${rom[2]}" thumbs
-    rclone mount thumbnails:${rom[2]}/Named_Snaps/ /userdata/roms/${rom[0]}/images --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf --vfs-cache-mode full --cache-dir=/userdata/system/.cache/rclone
+    rclone mount thumbnails:${rom[2]}/Named_Snaps/ /userdata/roms/${rom[0]}/images --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --config=/userdata/system/rclone.conf --http-no-head --cache-dir=/userdata/system/.cache/rclone
   fi  
   ls /userdata/roms/${rom[0]}/${rom3} | while read line; do
     if [[ ! ${line} =~ .*\.(jpg|png|torrent|xml|sqlite|mp3|ogg) ]]; then 
