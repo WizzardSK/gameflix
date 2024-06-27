2a49207c8af7bbb0285cb40e8e8f418cbbcee5e0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-27 21:29:03 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -24,7 +24,7 @@ for each in "${roms[@]}"; do
     mkdir -p ~/roms/${rom3}
     if [ -z "$(ls -A ~/roms/${rom3})" ]; then
       head ~/myrient/${rom[1]} > /dev/null
-      ~/ratarmount ~/myrient/${rom[1]} ~/roms/${rom3} --index-folders ~/share/system/.cache/ratarmount
+      ~/ratarmount ~/myrient/${rom[1]} ~/roms/${rom3} --index-folders ~/share/system/.cache/ratarmount &
     fi
   fi
 done
