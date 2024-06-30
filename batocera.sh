f571c2263f9478ba5532e324668627b61fd933ab
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-30 20:52:16 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -25,8 +25,9 @@ for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
   if [ ! -f /userdata/thumb/${rom[2]}.png ]; then wget -O /userdata/thumb/${rom[2]}.png https://raw.githubusercontent.com/libretro/retroarch-assets/master/xmb/monochrome/png/${rom[2]}.png; fi
 done
-{
+(
 for each in "${roms[@]}"; do
+  read -ra rom < <(printf '%s' "$each")
   mkdir -p /userdata/roms/${rom[0]}/images  
   if ! findmnt -rn /userdata/roms/${rom[0]}/images > /dev/null; then
     echo "${rom[2]}" thumbs
@@ -34,7 +35,7 @@ for each in "${roms[@]}"; do
     ls /userdata/roms/${rom[0]}/images > /dev/null
   fi
 done
-} &
+) &
 for each in "${roms[@]}"; do (
   read -ra rom < <(printf '%s' "$each")
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
