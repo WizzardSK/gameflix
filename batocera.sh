ed1b70bc7dfddf17cd061ea01c6310096748412d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-06-30 17:23:53 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -45,6 +45,7 @@ for each in "${roms[@]}"; do (
     echo "${rom[2]}" thumbs
     mount -o bind /userdata/thumbs/${rom[2]}/Named_Snaps /userdata/roms/${rom[0]}/images
     ls /userdata/roms/${rom[0]}/images > /dev/null
+    sleep 1
   fi
   if ! grep -Fxq "<gameList>" /userdata/roms/${rom[0]}/gamelist.xml > /dev/null; then
     ls /userdata/roms/${rom[0]}/${rom3} | while read line; do
