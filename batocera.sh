2d7277f3f60ec2905d1aaca6824d522410949e60
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-08-26 20:59:56 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -52,9 +52,8 @@ for each in "${roms[@]}"; do (
     echo "<folder><path>./${rom3}</path><name>${rom3}</name><image>~/../thumb/${rom[0]}.png</image></folder>" >> /userdata/roms/${rom[0]}/gamelist.xml
   fi
   rom2="${rom[2]// /_}"
-  if [ ! -d "/userdata/thumbs/${rom[2]}" ]; then
-    git clone "https://github.com/WizzardSK/${rom2}.git" /userdata/thumbs/${rom[2]}
-  fi
+  rom2="${rom[2]//+/}"
+  if [ ! -d "/userdata/thumbs/${rom[2]}" ]; then git clone "https://github.com/WizzardSK/${rom2}.git" /userdata/thumbs/${rom[2]}; fi
   git -C /userdata/thumbs/${rom[2]} checkout
   ) &
   sleep 1
