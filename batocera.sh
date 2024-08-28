a3f0ce208b336f84d4444096464f9bda36740cd0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2024-08-28 18:24:20 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -51,6 +51,7 @@ for each in "${roms[@]}"; do (
     echo "<folder><path>./${rom3}</path><name>${rom3}</name><image>~/../thumb/${rom[0]}.png</image></folder>" >> /userdata/roms/${rom[0]}/gamelist.xml
   fi
   ) &
+  sleep 1
 done
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each")
