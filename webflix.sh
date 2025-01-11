95863ba62b6c3108d0c208591d0fa9b54269ff3f
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-01-11 20:12:25 +0100

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -25,7 +25,7 @@ for each in "${roms[@]}"; do
     mkdir -p ~/roms/${rom3}
     if [ -z "$(ls -A ~/roms/${rom3})" ]; then
       if [ ! -f ~/share/zip/${rom3}.zip ]; then wget -O ~/share/zip/${rom3}.zip https://myrient.erista.me/files/${rom[1]}; fi
-      ~/ratarmount ~/share/zip/${rom3}.zip ~/share/roms/${rom[0]}/${rom3} --index-folders ~/share/system/.cache/ratarmount > /dev/null
+      ~/ratarmount ~/share/zip/${rom3}.zip ~/roms/${rom[0]}/${rom3} --index-folders ~/share/system/.cache/ratarmount > /dev/null
       #head ~/myrient/${rom[1]} > /dev/null
       #~/ratarmount ~/myrient/${rom[1]} ~/roms/${rom3} --index-folders ~/share/system/.cache/ratarmount > /dev/null &
     fi
