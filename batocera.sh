4f5553d476139112401170edd9f731ecf3a987db
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-01-13 22:29:02 +0100

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -44,7 +44,7 @@ for each in "${roms[@]}"; do
   if ! grep -Fxq "<gameList>" /userdata/roms/${rom[0]}/gamelist.xml > /dev/null 2>&1; then
     ls /userdata/roms/${rom[0]}/${rom3} | while read line; do
       line2=${line%.*}
-      hra="<game><path>./${rom3}/${line}</path><name>${line2}</name><image>~/../thumbs/${rom[2]}/Named_Snaps/${line2}.png</image><titleshot>~/../thumbs/${rom[2]}/Named_Titles/${line2}.png</titleshot><thumbnail>~/../thumbs/${rom[2]}/Named_Boxarts/${line2}.png</thumbnail><marquee>~/../thumbs/${rom[2]}/Named_Boxarts/${line2}.png</marquee>"
+      hra="<game><path>./${rom3}/${line}</path><name>${line2}</name><image>~/../thumbs/${rom[2]}/Named_Snaps/${line2}.png</image><titleshot>~/../thumbs/${rom[2]}/Named_Titles/${line2}.png</titleshot><thumbnail>~/../thumbs/${rom[2]}/Named_Boxarts/${line2}.png</thumbnail><marquee>~/../thumbs/${rom[2]}/Named_Logos/${line2}.png</marquee>"
       if ! grep -iqE '\[(bios|a[0-9]{0,2}|b[0-9]{0,2}|c|f|h ?.*|o ?.*|p ?.*|t ?.*|cr ?.*)\]|\((demo( [0-9]+)?|beta( [0-9]+)?|alpha( [0-9]+)?|pre-release|aftermarket|alt|alternate|unl|channel|system|dlc)\)' <<< "$line"; then
         echo "${hra}</game>" >> /userdata/roms/${rom[0]}/gamelist.xml
       else echo "${hra}<hidden>true</hidden></game>" >> /userdata/roms/${rom[0]}/gamelist.xml; fi    
