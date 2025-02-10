ddbf6a7c668b2ed1b629a467d2f771a47fae1eca
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-02-10 19:30:41 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -18,7 +18,7 @@ for each in "${roms[@]}"; do
     emufolder="${rom3}"
   else
     romfolder="myrient/${rom[1]}"
-    emufolder="$(basename "$(dirname "$rom[1]")")/$(basename "$rom[1]")"
+    emufolder=$(echo "$rom[1]" | rev | cut -d'/' -f1,2 | rev)
 #    emufolder="${rom[1]##*/}"
   fi
   if [ -e ~/gameflix/${rom3}.html ]; then
