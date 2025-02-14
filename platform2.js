961efaefa5868ccf95dd417b47b1cfe001453063
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-02-14 14:04:10 +0100

    Update platform2.js

--- platform2.js
+++ platform2.js
@@ -1,3 +1,3 @@
 fileNames.forEach(fileName => { 
-  document.write(`<a href="../roms/Tomy Tutor/${fileName}.bin" target="main"><figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/Tomy_-_Tutor/master/Named_Snaps/${fileName}.png" alt="${fileName}"><figcaption>${fileName}</figcaption></figure></a>`); 
+  document.write(`<a href="../$romfolder/${fileName}.bin" target="main"><figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/Tomy_-_Tutor/master/Named_Snaps/${fileName}.png" alt="${fileName}"><figcaption>${fileName}</figcaption></figure></a>`); 
 });
