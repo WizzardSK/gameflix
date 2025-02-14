440bf38534ac657aacf837e0350fd62b5a3bf2ac
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-02-14 17:16:36 +0100

    Delete platform2.js

--- platform2.js
+++
@@ -1,3 +0,0 @@
-fileNames.forEach(fileName => { 
-  document.write(`<a href="../$romfolder/${fileName}.bin" target="main"><figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/${rom[2]// /_}/master/Named_Snaps/${fileName}.png" alt="${fileName}"><figcaption>${fileName}</figcaption></figure></a>`);
-});
