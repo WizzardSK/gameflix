9973eaeab9922841f41234e8eac9087fd9510b4b
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-02-15 18:02:57 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -47,12 +47,11 @@ for each in "${roms[@]}"; do
     ((total++))
   done } < <(ls ~/${romfolder})
   echo "];" >> ~/gameflix/${rom3}.html
-  printf 'fileNames.forEach(fileName => { document.write(`<a href="../' >> ~/gameflix/${rom3}.html
+  printf 'generateFileLinks("' >> ~/gameflix/${rom3}.html
   printf $romfolder >> ~/gameflix/${rom3}.html
-  printf "/" >> ~/gameflix/${rom3}.html
-  printf '${fileName}" target="main"><figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/' >> ~/gameflix/${rom3}.html
+  printf '", "' >> ~/gameflix/${rom3}.html
   printf ${rom[2]// /_} >> ~/gameflix/${rom3}.html
-  echo '/master/Named_Snaps/${fileName.slice(0, fileName.lastIndexOf("."))}.png" alt="${fileName}"><figcaption>${fileName}</figcaption></figure></a>`); });' >> ~/gameflix/${rom3}.html
+  echo '");' >> ~/gameflix/${rom3}.html
   echo "</script></div><script src=\"script.js\"></script>" >> ~/gameflix/${rom3}.html
   echo "<a href=\"${rom3}.html\" target=\"main\">${rom[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html
   if [ "$platform" != "${rom[0]}" ]; then
