89f88418ccf91fa53fc28004db3af4d7762b87ec
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-03-06 17:30:54 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -22,8 +22,8 @@ for each in "${roms[@]}"; do
   fi
   if [[ "${rom[3]}" == *"eXoDOS"* ]]; then emufolder="roms/dos-other"; fi
 
-  if [ -e "~/gameflix/Atari 2600 ROMS.html" ]; then 
-    pocet=$(ls "~/roms/Atari 2600 ROMS" -1 | wc -l)
+  if [ -e ~/gameflix/Atari\ 2600\ ROMS.html ]; then 
+    pocet=$(ls ~/roms/Atari\ 2600\ ROMS -1 | wc -l)
     total=$((pocet+total))
     echo "<a href=\"Atari 2600 ROMS.html\" target=\"main\">Atari 2600 ROMS</a> ($pocet)<br />" >> ~/gameflix/systems.html
     echo "*\"Atari 2600 ROMS\") core=\"stella_libretro;;" >> ~/gameflix/retroarch.sh
