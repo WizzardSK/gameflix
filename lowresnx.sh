dcd3d89a2a5a17b903670e770ae35fe6e7b6e9e1
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-03-26 14:18:47 +0100

    Update lowresnx.sh

--- lowresnx.sh
+++ lowresnx.sh
@@ -21,3 +21,10 @@ while true; do
     echo "Stiahnutá stránka $page."
     ((page++))
 done
+
+git config --global user.name "GitHub Actions"
+git config --global user.email "actions@github.com"
+
+git add "$IMAGE_LIST"
+git commit -m "Automatická aktualizácia image_list.txt ($(date +'%Y-%m-%d %H:%M:%S'))"
+git push
