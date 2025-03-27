a6fff7b7ce332a26cfcad85b74c77d1c4b87da1d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-03-27 19:33:10 +0100

    Update lowresnx.sh

--- lowresnx.sh
+++ lowresnx.sh
@@ -3,26 +3,24 @@
 # URL na stiahnutie
 URL="https://lowresnx.inutilis.com/programs.php?category=game&sort=new&page=1"
 
-# Stiahnutie obsahu stránky do pamäte
-CONTENT=$(curl -s "$URL")
-
 # Cieľový CSV súbor
 CSV_FILE="output.csv"
 
 # Zápis hlavičky CSV
 echo "ID,Názov,Obrázok" > "$CSV_FILE"
 
-# Extrakcia ID
-IDs=$(echo "$CONTENT" | grep -oP 'topic.php\?id=\K[0-9]+')
-
-# Extrakcia názvov
-Titles=$(echo "$CONTENT" | grep -oP '<h3>\K[^<]+')
-
-# Extrakcia obrázkov
-Images=$(echo "$CONTENT" | grep -oP 'uploads/\K[^"]+')
-
-# Uloženie do CSV
-paste -d ',' <(echo "$IDs") <(echo "$Titles") <(echo "$Images") >> "$CSV_FILE"
+# Stiahnutie obsahu stránky a extrakcia údajov
+echo "$(curl -s "$URL")" | awk '
+    match($0, /topic.php\?id=([0-9]+)/, id) {
+        getline; getline;
+        match($0, /uploads\/([^\"]+)/, img);
+        getline; getline;
+        match($0, /<h3>([^<]+)/, title);
+        if (id[1] && title[1] && img[1]) {
+            print id[1] "," title[1] "," img[1];
+        }
+    }
+' >> "$CSV_FILE"
 
 echo "Hotovo! Dáta boli uložené do $CSV_FILE"
 
