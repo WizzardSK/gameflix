92281b028aed52e56f1746d48610a40c9cab9e10
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-03-28 14:46:21 +0100

    Update pico8.sh

--- pico8.sh
+++ pico8.sh
@@ -4,21 +4,15 @@ BASE_URL="https://www.lexaloffle.com/bbs/?cat=7&carts_tab=1#sub=2&page=1&mode=ca
 CSV_FILE="lexaloffle_data.csv"
 
 # Stiahnutie hlavnej stránky a získanie PID čísel
-echo "Fetching PID list..."
-pids=$(curl -s "$BASE_URL" | grep -oP 'pid=(\d+)' | grep -oP '\d+' | sort -u)
-
-echo "Found $(echo "$pids" | wc -l) PIDs"
+pids=$(curl -s "$BASE_URL" | grep -oP 'tid=(\d+)' | grep -oP '\d+' | sort -u)
 
 echo "PID" > "$CSV_FILE"
 
-# Zapísanie PID do CSV
 echo "$pids" | while read pid; do
     echo "$pid" >> "$CSV_FILE"
 done
 
 echo "Data saved to $CSV_FILE"
-
-
 
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
