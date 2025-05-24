8449a5977b5387480133dea8e69bac779c561ae5
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-05-24 09:29:27 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -20,9 +20,22 @@ curl "$FILE_URL" | while IFS=$'\t'; read -r id title image nx_file; do
   if [ ! -s "$DOWNLOAD_DIR/$nx_file" ]; then download_url="https://lowresnx.inutilis.com/uploads/$nx_file"; wget -nv "$download_url" -O "$DOWNLOAD_DIR/$nx_file"; fi
 done
 
+declare -A seen
+mkdir -p $HOME/thumbs
 IFS=$'\n' read -d '' -ra roms <<< "$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)"
 IFS=";"; for each in "${roms[@]}"; do
   echo "${rom3}"; read -ra rom < <(printf '%s' "$each")
+
+  if [[ -z "${seen[${rom[0]}]}" ]]; then
+    seen[${rom[0]}]=1; rom2="${rom[2]// /_}"; echo "${rom[2]} thumbs" | tee -a /var/log/git.log
+    if [ ! -d "$HOME/thumbs/${rom[2]}" ]; then git clone --depth 1 "https://github.com/WizzardSK/${rom2}.git" $HOME/thumbs/${rom[2]} 2>&1 | tee -a /var/log/git.log; else
+      git config --global --add safe.directory $HOME/thumbs/${rom[2]}
+      git -C $HOME/thumbs/${rom[2]} config pull.rebase false 2>&1 | tee -a /var/log/git.log
+      git -C $HOME/thumbs/${rom[2]} pull 2>&1 | tee -a /var/log/git.log
+      sleep 0.5
+    fi
+  fi  
+  
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
   if [[ ${rom[1]} =~ \.zip$ ]]; then
     mkdir -p ~/roms/${rom3}
@@ -31,5 +44,6 @@ IFS=";"; for each in "${roms[@]}"; do
     fi
   fi
 done
+
 wait
 
