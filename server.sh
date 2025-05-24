2d466e732c88960ac7aac8a173c8c212507a02a4
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-05-24 09:54:56 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -21,17 +21,17 @@ curl "$FILE_URL" | while IFS=$'\t'; read -r id title image nx_file; do
 done
 
 declare -A seen
-mkdir -p $HOME/thumbs
+mkdir -p $HOME/share/thumbs
 IFS=$'\n' read -d '' -ra roms <<< "$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)"
 IFS=";"; for each in "${roms[@]}"; do
   echo "${rom3}"; read -ra rom < <(printf '%s' "$each")
 
   if [[ -z "${seen[${rom[0]}]}" ]]; then
-    seen[${rom[0]}]=1; rom2="${rom[2]// /_}"; echo "${rom[2]} thumbs" | tee -a /var/log/git.log
-    if [ ! -d "$HOME/thumbs/${rom[2]}" ]; then git clone --depth 1 "https://github.com/WizzardSK/${rom2}.git" $HOME/thumbs/${rom[2]} 2>&1 | tee -a /var/log/git.log; else
-      git config --global --add safe.directory $HOME/thumbs/${rom[2]}
-      git -C $HOME/thumbs/${rom[2]} config pull.rebase false 2>&1 | tee -a /var/log/git.log
-      git -C $HOME/thumbs/${rom[2]} pull 2>&1 | tee -a /var/log/git.log
+    seen[${rom[0]}]=1; rom2="${rom[2]// /_}"; echo "${rom[2]} thumbs" | tee -a $HOME/git.log
+    if [ ! -d "$HOME/share/thumbs/${rom[2]}" ]; then git clone --depth 1 "https://github.com/WizzardSK/${rom2}.git" $HOME/share/thumbs/${rom[2]} 2>&1 | tee -a $HOME/git.log; else
+      git config --global --add safe.directory $HOME/share/thumbs/${rom[2]}
+      git -C $HOME/share/thumbs/${rom[2]} config pull.rebase false 2>&1 | tee -a $HOME/git.log
+      git -C $HOME/share/thumbs/${rom[2]} pull 2>&1 | tee -a $HOME/git.log
       sleep 0.5
     fi
   fi  
