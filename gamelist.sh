7fbd93e5012129685309ab4866ba7248b4bbc29c
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-05-27 15:00:41 +0200

    Update gamelist.sh

--- gamelist.sh
+++ gamelist.sh
@@ -3,22 +3,22 @@
 wget -O rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf > /dev/null 2>&1
 sudo -v ; curl https://rclone.org/install.sh | sudo bash  > /dev/null 2>&1
 sudo apt install fuse-zip  > /dev/null 2>&1
-mkdir -p rom roms zip zip/atari2600roms dos roms/neogeo 
+mkdir -p ~/rom ~/roms ~/zip ~/zip/atari2600roms ~/dos ~/roms/neogeo 
 rclone mount myrient: rom --config=rclone.conf --daemon
 
 IFS=$'\n' read -d '' -ra roms <<< "$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)"
-IFS=";"; for each in "${roms[@]}"; do read -ra rom < <(printf '%s' "$each"); mkdir -p roms/${rom[0]}; done
+IFS=";"; for each in "${roms[@]}"; do read -ra rom < <(printf '%s' "$each"); mkdir -p ~/roms/${rom[0]}; done
 for each in "${roms[@]}"; do 
   read -ra rom < <(printf '%s' "$each")
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
-  mkdir -p roms/${rom[0]}/${rom3}
-  if ! grep -Fxq "<gameList>" roms/${rom[0]}/gamelist.xml > /dev/null 2>&1; then
-    ls roms/${rom[0]}/${rom3} | while read line; do
+  mkdir -p ~/roms/${rom[0]}/${rom3}
+  if ! grep -Fxq "<gameList>" ~/roms/${rom[0]}/gamelist.xml > /dev/null 2>&1; then
+    ls ~/roms/${rom[0]}/${rom3} | while read line; do
       line2=${line%.*}
       hra="<game><path>./${rom3}/${line}</path><name>${line2}</name><image>~/../thumbs/${rom[2]}/Named_Snaps/${line2}.png</image><titleshot>~/../thumbs/${rom[2]}/Named_Titles/${line2}.png</titleshot><thumbnail>~/../thumbs/${rom[2]}/Named_Boxarts/${line2}.png</thumbnail><marquee>~/../thumbs/${rom[2]}/Named_Logos/${line2}.png</marquee>"
       if ! grep -iqE '\[(bios|a[0-9]{0,2}|b[0-9]{0,2}|c|f|h ?.*|o ?.*|p ?.*|t ?.*|cr ?.*)\]|\((demo( [0-9]+)?|beta( [0-9]+)?|alpha( [0-9]+)?|(disk|side)( [2-9B-Z]).*|pre-release|aftermarket|alt|alternate|unl|channel|system|dlc)\)' <<< "$line"; then
-        echo "${hra}</game>" >> roms/${rom[0]}/gamelist.xml
-      else echo "${hra}<hidden>true</hidden></game>" >> roms/${rom[0]}/gamelist.xml; fi    
+        echo "${hra}</game>" >> ~/roms/${rom[0]}/gamelist.xml
+      else echo "${hra}<hidden>true</hidden></game>" >> ~/roms/${rom[0]}/gamelist.xml; fi    
     done
     echo "<folder><path>./${rom3}</path><name>${rom3}</name><image>~/../thumb/${rom[0]}.png</image></folder>" >> ~/roms/${rom[0]}/gamelist.xml
   fi
@@ -26,8 +26,8 @@ done
 
 for each in "${roms[@]}"; do 
   read -ra rom < <(printf '%s' "$each")
-  if ! grep -Fxq "<gameList>" roms/${rom[0]}/gamelist.xml; then sed -i "1i <gameList>" roms/${rom[0]}/gamelist.xml; fi
-  if ! grep -Fxq "</gameList>" roms/${rom[0]}/gamelist.xml; then sed -i "\$a </gameList>" roms/${rom[0]}/gamelist.xml; fi
+  if ! grep -Fxq "<gameList>" ~/roms/${rom[0]}/gamelist.xml; then sed -i "1i <gameList>" ~/roms/${rom[0]}/gamelist.xml; fi
+  if ! grep -Fxq "</gameList>" ~/roms/${rom[0]}/gamelist.xml; then sed -i "\$a </gameList>" ~/roms/${rom[0]}/gamelist.xml; fi
 done
 
-ls
+ls ~
