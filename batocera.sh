576551fcad56456012eb5fba62bfcd497507cfb1
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-02 21:14:23 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -52,6 +52,7 @@ for each in "${roms[@]}"; do
   if [[ ${rom[1]} =~ \.zip$ ]]; then
     #if [ ! -f /userdata/zip/${rom3}.zip ]; then wget -O /userdata/zip/${rom3}.zip https://myrient.erista.me/files/${rom[1]}; fi
     #/userdata/system/fuse-zip /userdata/zip/${rom3}.zip /userdata/roms/${rom[0]}/${rom3}
+    head /userdata/rom/${rom[1]}
     /userdata/system/ratarmount /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/${rom3} --index-folders /userdata/system/.cache/ratarmount &
   else
     if grep -q ":" <<< "${rom[1]}"; then
