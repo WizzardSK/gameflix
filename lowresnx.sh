e8ae72739355d7ba314be584f61b8284ea8f1ab3
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-03 20:46:23 +0200

    Update lowresnx.sh

--- lowresnx.sh
+++ lowresnx.sh
@@ -8,18 +8,25 @@ while true; do
     echo "Page $page..."
     page_content=$(curl -s "$URL")
     if echo "$page_content" | grep -q "No results"; then break; fi
+    mkdir lowresnx
     echo "$page_content" | grep -oP 'topic.php\?id=\K[0-9]+' | while read id; do
         topic_page=$(curl -s "https://lowresnx.inutilis.com/topic.php?id=$id")
         title=$(echo "$topic_page" | grep -oP '(?<=<h1>).*?(?=</h1>)' | head -n 1)
         image=$(echo "$topic_page" | grep -oP '(?<=<img class="screenshot pixelated" src="uploads/)[^"]+')
         nx_url=$(echo "$topic_page" | grep -oP 'href="uploads/\K[^"]+\.nx' | head -n 1)
         echo -e "$id\t$title\t$image\t$nx_url" >> "$OUTPUT_FILE"
+        wget -nv -O lowresnx/$nx_url https://lowresnx.inutilis.com/upload/$nx_url
     done
     ((page++)) 
 done
 
+rm lowresnx.zip
+cd ~/lowresnx
+zip -r "$GITHUB_WORKSPACE/lowresnx.zip" *
+cd "$GITHUB_WORKSPACE"
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git add "$OUTPUT_FILE"
+git add lowresnx.zip
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
 git push
