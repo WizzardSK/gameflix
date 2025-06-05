6f1cc7bfaa559e803cfc10c70cfc9180641a5017
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-05 19:48:28 +0200

    Create tic80.sh

---
+++ tic80.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+API_URL="https://tic80.com/api?fn=dir&path=play/Games"
+BASE_URL="https://tic80.com/cart"
+DOWNLOAD_DIR="$HOME/tic80"
+mkdir $DOWNLOAD_DIR
+RESPONSE=$(curl -s "$API_URL")
+FILES=$(echo "$RESPONSE" | grep -oP '{\s*name\s*=\s*"[^"]+",\s*hash\s*=\s*"[^"]+",\s*id\s*=\s*\d+,\s*filename\s*=\s*"[^"]+"\s*}')
+echo "$FILES" | while read -r LINE; do
+    HASH=$(echo "$LINE" | sed -n 's/.*hash\s*=\s*"\([^"]*\)".*/\1/p')
+    FILENAME=$(echo "$LINE" | sed -n 's/.*filename\s*=\s*"\([^"]*\)".*/\1/p')
+    FILE_PATH="${DOWNLOAD_DIR}/${HASH} ${FILENAME}"; DOWNLOAD_URL="${BASE_URL}/${HASH}/cart.tic"
+    if [ ! -f "$FILE_PATH" ]; then wget -O "$FILE_PATH" "$DOWNLOAD_URL"; fi
+done
+
+rm tic80.zip
+cd $DOWNLOAD_DIR
+zip -r "$GITHUB_WORKSPACE/tic80.zip" *
+cd "$GITHUB_WORKSPACE"
+git config --global user.name "GitHub Actions"
+git config --global user.email "actions@github.com"
+git add tic80.zip
+git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
+git push
