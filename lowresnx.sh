953d9cf10c46aa4489a325ca2d5c15556ec3c6c6
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-05 21:11:11 +0200

    Update lowresnx.sh

--- lowresnx.sh
+++ lowresnx.sh
@@ -16,7 +16,7 @@ while true; do
         nx_url=$(echo "$topic_page" | grep -oP 'href="uploads/\K[^"]+\.nx' | head -n 1)
         echo -e "$id\t$title\t$image\t$nx_url" >> "$OUTPUT_FILE"
         wget -nv -O ~/lowresnx/$nx_url https://lowresnx.inutilis.com/upload/$nx_url
-        wget -nv -O ~/lowresnx/$nx_url https://lowresnx.inutilis.com/upload/$image
+        wget -nv -O ~/lowresnx/$image https://lowresnx.inutilis.com/upload/$image
     done
     ((page++)) 
 done
