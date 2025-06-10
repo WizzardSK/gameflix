7604977bb88798330bd69fbc90edf8e1e109b7cd
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-10 18:32:44 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=/usr/local/lib
-mkdir -p ~/myrient ~/roms ~/dos ~/iso ~/gameflix ~/share/system/.cache/ratarmount ~/share/system/.cache/rclone ~/share/zip/atari2600roms ~/roms/Atari\ 2600\ ROMS ~/roms/TIC-80 ~/roms/Uzebox ~/roms/WASM-4
+mkdir -p ~/myrient ~/roms ~/dos ~/iso ~/zips ~/gameflix ~/share/system/.cache/ratarmount ~/share/system/.cache/rclone ~/share/zip/atari2600roms ~/roms/Atari\ 2600\ ROMS ~/roms/TIC-80 ~/roms/Uzebox ~/roms/WASM-4
 
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 rclone mount myrient: ~/myrient --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other
-#rclone mount myrient:TOSEC ~/myrient/TOSEC --vfs-cache-mode full --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other
 
 IFS=$'\n' read -d '' -ra roms <<< "$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)"
 IFS=";"; for each in "${roms[@]}"; do
@@ -16,12 +15,14 @@ IFS=";"; for each in "${roms[@]}"; do
   fi
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
   if [[ ${rom[1]} =~ \.zip$ ]]; then
-    mkdir -p ~/roms/${rom3}
-    if [ -z "$(ls -A ~/roms/${rom3})" ]; then
-      ratarmount https://myrient.erista.me/files/${rom[1]} ~/roms/${rom3} -f &
-      sleep 3
-    fi
+    archives+=" ${rom[1]}"
+#    mkdir -p ~/roms/${rom3}
+#    if [ -z "$(ls -A ~/roms/${rom3})" ]; then
+#      ratarmount https://myrient.erista.me/files/${rom[1]} ~/roms/${rom3} -f &
+#    fi
   fi
 done
+
+ratarmount --disable-union-mount $archives ~/zips -f &
 
 wait
