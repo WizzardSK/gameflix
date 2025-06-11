af21d7a5437d40e9f300dc2686e824f08edb8271
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-11 09:26:39 +0200

    Update wasm4.sh

--- wasm4.sh
+++ wasm4.sh
@@ -8,13 +8,12 @@ mkdir -p "$ROM_DIR" "$IMG_DIR"; curl -s "$BASE_URL" | grep -oP '(?<=href="/play/
   for EXT in wasm png; do FILE="${ROM_DIR}/$GAME.$EXT"; [[ "$EXT" == "png" ]] && FILE="${IMG_DIR}/$GAME.$EXT"; [[ -f "$FILE" ]] || wget -nv -O "$FILE" "$CARTS_URL/$GAME.$EXT"; done
 done
 
-rm wasm4.zip
 cd ~/wasm4
-zip -r "$GITHUB_WORKSPACE/wasm4.zip" *
+zip -r "$GITHUB_WORKSPACE/fantasy/wasm4.zip" *
 cd "$GITHUB_WORKSPACE"
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git add "$OUTPUT_FILE"
-git add wasm4.zip
+git add "$GITHUB_WORKSPACE/fantasy/wasm4.zip"
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
 git push
