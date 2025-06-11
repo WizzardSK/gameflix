e7582d974cf4a46e7cfdf2841079a2ce6b65cbc7
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-11 13:49:19 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export LD_LIBRARY_PATH=/usr/local/lib
 mkdir -p ~/myrient ~/roms ~/dos ~/iso ~/zips ~/gameflix ~/share/system/.cache/ratarmount ~/share/system/.cache/rclone ~/share/zip/atari2600roms ~/roms/Atari\ 2600\ ROMS ~/roms/TIC-80 ~/roms/Uzebox ~/roms/WASM-4
 
@@ -14,20 +13,13 @@ IFS=";"; for each in "${roms[@]}"; do
     rclone mount ${rom[1]} ~/${rom[0]} --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other
   fi
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
+  archives="https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip"
   if [[ ${rom[1]} =~ \.zip$ ]]; then
-    rom[1]="${rom[1]//&/%26}"    
-    rom[1]="${rom[1]// /%20}"
-    rom[1]="${rom[1]//[/%5B}"
-    rom[1]="${rom[1]//]/%5D}"
-    rom[1]="${rom[1]//\'/%27}"
+    rom[1]="${rom[1]//&/%26}"; rom[1]="${rom[1]// /%20}"; rom[1]="${rom[1]//[/%5B}"; rom[1]="${rom[1]//]/%5D}"; rom[1]="${rom[1]//\'/%27}"
     archives+=" https://myrient.erista.me/files/${rom[1]}"
-#    mkdir -p ~/roms/${rom3}
-#    if [ -z "$(ls -A ~/roms/${rom3})" ]; then
-#      ratarmount https://myrient.erista.me/files/${rom[1]} ~/roms/${rom3} -f &
-#    fi
   fi
 done
 
-ratarmount --disable-union-mount "$archives" ~/zips -f &
+ratarmount --disable-union-mount $archives ~/zips -f &
 
 wait
