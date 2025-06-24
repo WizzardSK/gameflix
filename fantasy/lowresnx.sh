8b12dc1fa4a6a4815847ade239e7094fdc0428e8
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-06-24 09:19:59 +0200

    Update lowresnx.sh

--- fantasy/lowresnx.sh
+++ fantasy/lowresnx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASE_URL="https://lowresnx.inutilis.com/programs.php?category=game&sort=new&page="
-OUTPUT_FILE="lowresnx.txt"
+OUTPUT_FILE="$GITHUB_WORKSPACE/fantasy/lowresnx.zip"
 > "$GITHUB_WORKSPACE/fantasy/$OUTPUT_FILE"
 page=1
 mkdir ~/lowresnx
