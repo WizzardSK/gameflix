01008570fbf8941750fb38d8df6fe3ddc011d626
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-14 13:20:16 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -6,5 +6,16 @@ mkdir -p $HOME/.config/rclone
 cp rclone.conf $HOME/.config/rclone/
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
 sudo apt install bindfs > /dev/null
+
 bash ./webflix.sh
 bash ./generate.sh
+
+cd ~/gameflix
+rm -f "$GITHUB_WORKSPACE/gameflix.zip"
+zip -r "$GITHUB_WORKSPACE/gameflix.zip" *
+cd "$GITHUB_WORKSPACE"
+git config --global user.name "GitHub Actions"
+git config --global user.email "actions@github.com"
+git add "$GITHUB_WORKSPACE/gameflix.zip"
+git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
+git push
