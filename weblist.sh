2d54274090fae448dee9592d6a120921f119ac26
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-14 13:09:42 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -5,5 +5,6 @@ sudo ln -s $GITHUB_WORKSPACE/batocera/ratarmount1 /bin/ratarmount
 mkdir -p $HOME/.config/rclone
 cp rclone.conf $HOME/.config/rclone/
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
+sudo apt install bindfs
 bash ./webflix.sh
 bash ./generate.sh
