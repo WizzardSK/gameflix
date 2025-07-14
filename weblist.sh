56e5b5dcba4259537faf2680fd4689869e638745
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-14 08:55:33 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -2,5 +2,6 @@
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 ln -s batocera/ratarmount1 /usr/bin/ratarmount
 chmod +x /usr/bin/ratarmount
+ln -s rclone.conf /home/runner/.config/rclone/rclone.conf
 bash ./webflix.sh
 bash ./generate.sh
