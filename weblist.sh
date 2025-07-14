e81c5d95ce9b7516765857c1d74fe2a5acc43b5d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-14 13:05:35 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo -v ; curl https://rclone.org/install.sh | sudo bash > /dev/null
-chmod +x $HOME/batocera/ratarmount1
-sudo ln -s $HOME/batocera/ratarmount1 /bin/ratarmount
+chmod +x $GITHUB_WORKSPACE/batocera/ratarmount1
+sudo ln -s $GITHUB_WORKSPACE/batocera/ratarmount1 /bin/ratarmount
 mkdir -p $HOME/.config/rclone
 cp rclone.conf $HOME/.config/rclone/
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
