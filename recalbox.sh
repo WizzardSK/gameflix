a69237ed5c82ba83f589ec569c9749d6b3e735dd
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-17 13:18:14 +0200

    Update recalbox.sh

--- recalbox.sh
+++ recalbox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mount -o remount,rw /
 ln -s /usr/bin/fusermount /usr/bin/fusermount3
+mkdir -p /recalbox/share/{rom,roms,thumbs,zip,romz}
 
 case $( uname -m ) in
   armv7l) ziparch="arm"; rclarch="arm-v7" ;;
@@ -9,22 +10,21 @@ case $( uname -m ) in
   i386) ziparch="ia32"; rclarch="386" ;;
 esac
 
-if [ ! -f /usr/bin/7za ]; then wget -O /usr/bin/7za https://github.com/develar/7zip-bin/raw/master/linux/${ziparch}/7za; chmod +x /usr/bin/7za; fi
-if [ ! -f /usr/bin/mount-zip ]; then wget -O /usr/bin/mount-zip https://github.com/WizzardSK/gameflix/raw/main/recalbox/mount-zip; chmod +x /usr/bin/mount-zip; fi
+if [ ! -f /usr/bin/7za ]; then wget -nv -O /usr/bin/7za https://github.com/develar/7zip-bin/raw/master/linux/${ziparch}/7za; chmod +x /usr/bin/7za; fi
+if [ ! -f /usr/bin/mount-zip ]; then wget -nv -O /usr/bin/mount-zip https://github.com/WizzardSK/gameflix/raw/main/recalbox/mount-zip; chmod +x /usr/bin/mount-zip; fi
 if [ ! -f /usr/bin/rclone ]; then
-  wget -O /usr/bin/rclone.zip https://downloads.rclone.org/v1.65.0/rclone-v1.65.0-linux-${rclarch}.zip
+  wget -nv -O /usr/bin/rclone.zip https://downloads.rclone.org/v1.65.0/rclone-v1.65.0-linux-${rclarch}.zip
   7za e -y /usr/bin/rclone.zip
   mv rclone /usr/bin
   chmod +x /usr/bin/rclone
   rm /usr/bin/rclone.zip
 fi
 
-wget -O /recalbox/share_init/system/.emulationstation/systemlist.xml https://github.com/WizzardSK/gameflix/raw/main/recalbox/systemlist.xml
-wget -O /recalbox/share/system/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
+wget -nv -O /recalbox/share_init/system/.emulationstation/systemlist.xml https://github.com/WizzardSK/gameflix/raw/main/recalbox/systemlist.xml
+wget -nv -O /recalbox/share/system/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 IFS=$'\n' read -d '' -ra roms <<< "$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.txt)"
 
 es stop; chvt 3; clear
-mkdir -p /recalbox/share/{rom,roms,thumbs,zip,romz}
 
 rclone mount thumbnails: /recalbox/share/thumbs --config=/recalbox/share/system/rclone.conf --daemon --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty
 rclone mount myrient: /recalbox/share/rom --config=/recalbox/share/system/rclone.conf --daemon --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty
