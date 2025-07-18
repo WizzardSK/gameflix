b4ce32f0db17bbde4f73019c35b65fdbbcce9a9e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-18 21:38:42 +0200

    Update platform.js

--- platform.js
+++ platform.js
@@ -35,26 +35,29 @@ function generateTicLinks(romPath, imagePath) {
 }
 
 function generateWasmLinks(romPath, imagePath) {
-    document.write("<div id=\"figureList\">"); 
+    document.write("<div id=\"figureList\">");
     if (location.protocol !== "file:" && romPath.includes("roms/WASM-4")) { romPath = romPath.replace("roms/WASM-4", "https://wasm4.org/play"); wasm = ``; } else { romPath = `../${romPath}`; wasm = `.wasm`; }
     fileNames.forEach(fileName => {
-        const [subor, nazov] = fileName.split(',');
+        const [subor, nazov] = fileName.split('\t');
         document.write(`<a href="${romPath}/${encodeURIComponent(subor)}${wasm}" target="main">
         <figure><img loading="lazy" src="https://wasm4.org/carts/${subor}.png" alt="${nazov}"><figcaption>${nazov}</figcaption></figure></a>`);
     }); document.write("</div>");
 }
 
 function generateLrNXLinks(romPath, imagePath) {
-    document.write("<div id=\"figureList\">"); fileNames.forEach(fileName => {
-        const [subor, obrazok, nazov] = fileName.split('|'); document.write(`<a href="../${romPath}/${encodeURIComponent(subor)}" target="main">
+    document.write("<div id=\"figureList\">");
+    if (location.protocol !== "file:" && romPath.includes("roms/LowresNX")) { romPath = romPath.replace("roms/LowresNX", "https://lowresnx.inutilis.com/topic.php?id="); web = true; } 
+    else { romPath = `../${romPath}/`; web = false; }
+    fileNames.forEach(fileName => {
+        let [subor, obrazok, nazov, id] = fileName.split('\t'); if (web) { subor = `${id}`; }
+        document.write(`<a href="${romPath}${encodeURIComponent(subor)}" target="main">
         <figure><img loading="lazy" src="https://lowresnx.inutilis.com/uploads/${obrazok}" alt="${nazov}"><figcaption>${nazov}</figcaption></figure></a>`);
     }); document.write("</div>");
 }
 
 function generatePicoLinks(romPath, imagePath) {
     document.write("<div id=\"figureList\">"); fileNames.forEach(fileName => {
-        const [id, nazov, kart] = fileName.split('\t');
-        let screen;
+        const [id, nazov, kart] = fileName.split('\t'); let screen;
         if (/^\d/.test(kart)) { screen = "pico" + kart.replace(/\.p8\.png$/, '.png'); } else { screen = kart.replace(/^(.*)\.p8\.png$/, 'pico8_$1.png'); }
         let cart = kart.replace(/\.p8.png$/, "");
         document.write(`<a href="https://www.lexaloffle.com/bbs/?pid=${cart}#p" target="main">
@@ -82,13 +85,13 @@ function generateUzeLinks(romPath, imagePath) {
 function generateFileLinks(romPath, imagePath) {
     document.write("<div id=\"figureList\">");
     if (location.protocol !== "file:" && romPath.includes("eXoDOS")) { romPath = romPath.replace("myrient/../roms/MS-DOS eXoDOS", "https://the-eye.eu/public/Games/eXo/eXoDOS_v6r2/eXo/eXoDOS"); }
-    if (location.protocol !== "file:" && romPath.startsWith("myrient/")) { romPath = romPath.replace(/^myrient\/?/, "https://myrient.erista.me/files/"); }
-    fileNames.forEach(fileName => {
-        const subor = fileName.includes("\t") ? fileName.split("\t")[0] : fileName;
-        const nameWithoutExt = subor.slice(0, subor.lastIndexOf(".")) || subor;
-        const nazov = fileName.includes("\t") ? fileName.split("\t")[1] : fileName.replace(/\.[^.]+$/, "");
-        document.write(`<a href="${romPath}/${encodeURIComponent(subor)}" target="main">
-        <figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/${imagePath}/master/Named_Snaps/${encodeURIComponent(nameWithoutExt)}.png" alt="${nameWithoutExt}"><figcaption>${nazov}</figcaption></figure></a>`);
-    });
-    document.write("</div>");
+        if (location.protocol !== "file:" && romPath.startsWith("myrient/")) { romPath = romPath.replace(/^myrient\/?/, "https://myrient.erista.me/files/"); }
+            fileNames.forEach(fileName => {
+                const subor = fileName.includes("\t") ? fileName.split("\t")[0] : fileName;
+                const nameWithoutExt = subor.slice(0, subor.lastIndexOf(".")) || subor;
+                const nazov = fileName.includes("\t") ? fileName.split("\t")[1] : fileName.replace(/\.[^.]+$/, "");
+                document.write(`<a href="${romPath}/${encodeURIComponent(subor)}" target="main">
+                <figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/${imagePath}/master/Named_Snaps/${encodeURIComponent(nameWithoutExt)}.png" alt="${nameWithoutExt}"><figcaption>${nazov}</figcaption></figure></a>`);
+            });
+            document.write("</div>");
 }
