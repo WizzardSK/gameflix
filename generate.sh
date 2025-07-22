c875869cd05bb47f7e8f5fbdfb314152363cc466
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-22 22:26:26 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -80,9 +80,9 @@ IFS=";"
 for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each"); rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
   romfolder="myrient/${rom[1]}"; emufolder="${rom[1]}";
-  if [[ "${rom[3]}" == *"eXoDOS"* ]]; then romfolder="roms/MS-DOS eXoDOS"; emufolder="roms/MS-DOS eXoDOS"; fi
-  if [[ "${rom[3]}" == *"roms/TDC"* ]]; then romfolder="roms/TDC"; emufolder="roms/TDC"; fi
-  if [[ "${rom[3]}" == *"roms/C64"* ]]; then romfolder="roms/C64"; emufolder="roms/C64"; fi
+  if [[ "${rom[1]}" == *"eXoDOS"* ]]; then romfolder="roms/MS-DOS eXoDOS"; emufolder="roms/MS-DOS eXoDOS"; fi
+  if [[ "${rom[1]}" == *"roms/TDC"* ]]; then romfolder="roms/TDC"; emufolder="roms/TDC"; fi
+  if [[ "${rom[1]}" == *"roms/C64"* ]]; then romfolder="roms/C64"; emufolder="roms/C64"; fi
   if [ -e ~/gameflix/${rom3}.html ]; then
     pocet=$(ls ~/${romfolder} -1 | wc -l); total=$((pocet+total))
     echo "<a href=\"${rom3}.html\" target=\"main\">${rom[3]}</a> ($pocet)<br />" >> ~/gameflix/systems.html
