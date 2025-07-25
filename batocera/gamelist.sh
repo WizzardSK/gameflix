da753d8957944791248ea2fa3a004f842ac5f5d0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-25 12:28:40 +0200

    Update gamelist.sh

--- batocera/gamelist.sh
+++ batocera/gamelist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 shopt -s nocasematch
 sudo -v ; curl https://rclone.org/install.sh | sudo bash > /dev/null
-mkdir -p ~/{rom,roms,zip,zips,atari2600roms,mount,uzebox,vircon32} ~/roms/{neogeo,uzebox,tic80,wasm4,lowresnx,vircon32,pico8,voxatron,dos}
+mkdir -p ~/{rom,roms,zip,zips,atari2600roms,mount,uzebox,vircon32} ~/roms/{neogeo,uzebox,tic80,wasm4,lowresnx,vircon32,pico8,voxatron,dos} ~/mount/dos/MS-DOS\ eXoDOS
 sudo apt install fuse-zip > /dev/null
 rclone mount myrient: ~/rom --config=rclone.conf --daemon --http-no-head
 rclone mount archive:all_vircon32_roms_and_media/all_vircon32_roms_and_media ~/vircon32 --daemon --config=rclone.conf
