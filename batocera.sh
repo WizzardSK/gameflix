185886d2aae9ada322d9a9de8c7cdaec394f5313
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-25 13:50:15 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -24,26 +24,11 @@ else
   archives+=( /userdata/zip/{tic80,wasm4,uzebox,lowresnx}.zip )
 fi
 
-IFS=";"
-for each in "${roms[@]}"; do 
+IFS=";"; for each in "${roms[@]}"; do 
   read -ra rom < <(printf '%s' "$each")
   if [ ! -f /userdata/thumb/${rom[0]}.png ]; then wget -nv -O /userdata/thumb/${rom[0]}.png https://raw.githubusercontent.com/fabricecaruso/es-theme-carbon/master/art/consoles/${rom[0]}.png; fi
   rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}"); mkdir -p /userdata/roms/${rom[0]}/${rom3}
-#  if [[ ${rom[1]} =~ \.zip$ ]]; then
-#    rom1="${rom[1]//&/%26}"; rom1="${rom1// /%20}"; rom1="${rom1//[/%5B}"; rom1="${rom1//]/%5D}"; rom1="${rom1//\'/%27}"
-#    if [ ! -f /userdata/system/offline ]; then
-#      archives+=( "https://myrient.erista.me/files/${rom1}" )
-#    else
-#      romfile=$(basename "${rom1}")
-#      if [ ! -f /userdata/zip/${romfile} ]; then wget -O /userdata/zip/${romfile} https://myrient.erista.me/files/${rom1}; fi
-#      archives+=( "/userdata/zip/${romfile}" )
-#    fi
-#  else
-#    if grep -q ":" <<< "${rom[1]}"; then
-#      rclone mount ${rom[1]} /userdata/roms/${rom[0]}/${rom3} --http-no-head --no-checksum --no-modtime --dir-cache-time 1000h --allow-non-empty --attr-timeout 1000h --poll-interval 1000h --daemon --config=/userdata/system/rclone.conf
-#    else
-  mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/${rom3}; fi
-#  fi
+  mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/${rom3}
 done
 
 /userdata/system/ratarmount -o attr_timeout=3600 --disable-union-mount "${archives[@]}" /userdata/zips -f & 
@@ -54,14 +39,10 @@ mount -o bind /userdata/zips/lowresnx.zip "/userdata/roms/lowresnx/LowresNX"
 mount -o bind /userdata/zips/wasm4.zip "/userdata/roms/wasm4/WASM-4"
 mount -o bind /userdata/zips/uzebox.zip "/userdata/roms/uzebox/Uzebox"
 
-for each in "${roms[@]}"; do
-  read -ra rom < <(printf '%s' "$each")
-  rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
-#  if [[ ${rom[1]} =~ \.zip$ ]]; then
-#    rom1="${rom[1]//&/%26}"; rom1="${rom1// /%20}"; rom1="${rom1//[/%5B}"; rom1="${rom1//]/%5D}"; rom1="${rom1//\'/%27}"
-#    mkdir -p /userdata/roms/${rom[0]}/${rom3}; mount -o bind /userdata/zips/${rom1##*/} /userdata/roms/${rom[0]}/${rom3}
-#  fi
-done
+#for each in "${roms[@]}"; do
+#  read -ra rom < <(printf '%s' "$each")
+#  rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}")
+#done
 
 DAT_URL="https://github.com/WizzardSK/gameflix/raw/refs/heads/main/neogeo.dat"; DAT_FILE="/tmp/neogeo.dat"
 SRC_DIR="/userdata/rom/Internet Archive/chadmaster/fbnarcade-fullnonmerged/arcade"; DEST_DIR="/userdata/roms/neogeo/Neo Geo"
