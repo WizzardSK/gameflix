5b3075ad5fddf27ade8e32060e7253dbc0ae7756
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-26 08:54:16 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -6,7 +6,7 @@ if ! mountpoint -q "$HOME/myrient"; then rclone mount myrient: ~/myrient --http-
 if ! mountpoint -q "$HOME/MS-DOS eXoDOS"; then rclone mount eye:Games/eXo/eXoDOS_v6r2/eXo/eXoDOS/ "$HOME/roms/MS-DOS eXoDOS" --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
 
 archives=( "https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" )
-archives+=( https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/{lowresnx,tic80,wasm4,uzebox}.zip )
+archives+=( https://wizzardsk.github.io/{lowresnx,tic80,wasm4,uzebox}.zip )
 
 if ! mountpoint -q "$HOME/zips"; then nohup ratarmount -o attr_timeout=3600 --disable-union-mount "${archives[@]}" ~/zips -f & fi
 if ! mountpoint -q "$HOME/roms/Vircon32"; then rclone mount archive:all_vircon32_roms_and_media/all_vircon32_roms_and_media $HOME/roms/Vircon32 --daemon; fi
