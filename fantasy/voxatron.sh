a6050f2b836fab8a632b05432d5b38c64529e6d2
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-27 10:32:17 +0200

    Update voxatron.sh

--- fantasy/voxatron.sh
+++ fantasy/voxatron.sh
@@ -3,7 +3,6 @@ OUTPUT_FILE="$GITHUB_WORKSPACE/fantasy/voxatron.txt"
 TEMP_FILE="voxatron_temp.txt"
 BASE_LIST_URL="https://www.lexaloffle.com/bbs/lister.php?cat=6&sub=2&mode=carts&page="
 BASE_CART_URL="https://www.lexaloffle.com/bbs/?tid="
-mkdir ~/voxatron
 PAGE=1; > "$TEMP_FILE"
 while true; do
     echo "➡️ Page $PAGE..."
@@ -15,7 +14,6 @@ while true; do
         CART_HTML=$(curl -s "${BASE_CART_URL}${TID}")
         PNG_NAME=$(echo "$CART_HTML" | grep -oP 'href="[^"]+/(?:[^"/]+\.vx\.png|cpost[0-9]+\.png)"' | head -n1 | sed -E 's/.*\/([^/]+\.png)".*/\1/')
         PNG_URL=$(echo "$CART_HTML" | grep -oP 'href="\K[^"]+/(?:[^"/]+\.vx\.png|cpost[0-9]+\.png)' | head -n1)
-        #wget -nv -O ~/voxatron/$PNG_NAME "https://www.lexaloffle.com${PNG_URL}"
         if [[ -z "$PNG_NAME" ]]; then continue; fi
         echo -e "$TID\t$TITLE\t$PNG_NAME" >> "$TEMP_FILE"
     done
@@ -24,12 +22,9 @@ while true; do
 done
 sort -nr "$TEMP_FILE" > "$OUTPUT_FILE"
 rm -f "$TEMP_FILE"
-cd ~/voxatron
-#zip -r "$GITHUB_WORKSPACE/fantasy/voxatron.zip" *
 cd "$GITHUB_WORKSPACE"
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git add "$OUTPUT_FILE"
-#git add "$GITHUB_WORKSPACE/fantasy/voxatron.zip"
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
 git push
