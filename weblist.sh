2407d2f1ab0d790ccd64c48c83e9b4f2f81394b2
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-01 22:03:14 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-bash ./batocera/gamelist.sh
-
 sudo -v ; curl https://rclone.org/install.sh | sudo bash > /dev/null
 chmod +x $GITHUB_WORKSPACE/batocera/ratarmount1
 sudo ln -s $GITHUB_WORKSPACE/batocera/ratarmount1 /bin/ratarmount
@@ -10,6 +7,7 @@ cp rclone.conf $HOME/.config/rclone/
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
 sudo apt install bindfs > /dev/null
 
+bash ./batocera/gamelist.sh
 bash ./webflix.sh
 bash ./generate.sh
 
