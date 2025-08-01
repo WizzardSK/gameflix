84d705c6117d895d8c150168587bbc379ea5d1f2
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-01 21:26:30 +0200

    Update weblist.sh

--- weblist.sh
+++ weblist.sh
@@ -19,3 +19,5 @@ git config --global user.email "actions@github.com"
 git add "$GITHUB_WORKSPACE/gameflix.zip"
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
 git push
+
+bash ./batocera/gamelist.sh
