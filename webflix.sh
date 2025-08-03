1f383b1ef4c83f268618c8d006397eb997947764
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-03 22:34:58 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -2,8 +2,7 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 mkdir -p ~/myrient ~/roms/{Atari\ 2600\ ROMS,TIC-80,LowresNX,Uzebox,WASM-4,Vircon32,MS-DOS\ eXoDOS} ~/iso ~/zips ~/gameflix
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
-archives=( "https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" )
-archives+=( https://wizzardsk.github.io/{lowresnx,tic80,wasm4,uzebox}.zip )
+archives=("https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" https://wizzardsk.github.io/{lowresnx,tic80,wasm4,uzebox})
 
 if ! mountpoint -q "$HOME/myrient"; then rclone mount myrient: ~/myrient --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
 if ! mountpoint -q "$HOME/roms/MS-DOS eXoDOS"; then rclone mount eye:Games/eXo/eXoDOS_v6r2/eXo/eXoDOS/ "$HOME/roms/MS-DOS eXoDOS" --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
@@ -21,8 +20,7 @@ DAT_URL="https://github.com/WizzardSK/gameflix/raw/refs/heads/main/neogeo.dat"; 
 SRC_DIR="$HOME/myrient/Internet Archive/chadmaster/fbnarcade-fullnonmerged/arcade"; DEST_DIR="$HOME/roms/Neo Geo"
 curl -s -L "$DAT_URL" -o "$DAT_FILE"; mkdir -p "$DEST_DIR"
 while IFS= read -r line; do
-  neo_file="${line%%[[:space:]]*}"
-  [[ -z "$neo_file" || "$neo_file" != *.neo ]] && continue
+  neo_file="${line%%[[:space:]]*}"; [[ -z "$neo_file" || "$neo_file" != *.neo ]] && continue
   base_name="${neo_file%.neo}"; zip_name="$base_name.zip"; src_file="$SRC_DIR/$zip_name"; dest_link="$DEST_DIR/$zip_name"
   if [[ -f "$src_file" ]]; then ln -sf "$src_file" "$dest_link"; fi
 done < "$DAT_FILE"
