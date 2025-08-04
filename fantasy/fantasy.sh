0a3b736cfd92e3239f0da612668c147797a3c087
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-04 22:00:40 +0200

    Update fantasy.sh

--- fantasy/fantasy.sh
+++ fantasy/fantasy.sh
@@ -2,9 +2,9 @@
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 
-bash ./fantasy/wasm4.sh
+#bash ./fantasy/wasm4.sh
 #bash ./fantasy/voxatron.sh
-#bash ./fantasy/tic80.sh
+bash ./fantasy/tic80.sh
 #bash ./fantasy/pico8.sh
 #bash ./fantasy/lowresnx.sh
 
