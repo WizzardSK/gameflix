5b74b35f1760d0a7920933bb7a4464d3d9e5c1fe
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-04 21:14:16 +0200

    Update fantasy.sh

--- fantasy/fantasy.sh
+++ fantasy/fantasy.sh
@@ -3,10 +3,10 @@ git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 
 bash ./fantasy/wasm4.sh
-bash ./fantasy/voxatron.sh
-bash ./fantasy/tic80.sh
-bash ./fantasy/pico8.sh
-bash ./fantasy/lowresnx.sh
+#bash ./fantasy/voxatron.sh
+#bash ./fantasy/tic80.sh
+#bash ./fantasy/pico8.sh
+#bash ./fantasy/lowresnx.sh
 
 git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
 git push
