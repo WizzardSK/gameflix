96f45848c517dca600e944bec3db0796f3b70bf1
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-04 14:13:57 +0200

    Update batocera.sh

--- batocera.sh
+++ batocera.sh
@@ -14,14 +14,12 @@ rclone mount thumbs:Data/share/thumbs /userdata/thumbs --vfs-cache-mode full --d
 rclone mount archive:all_vircon32_roms_and_media/all_vircon32_roms_and_media /userdata/roms/vircon32/Vircon32 --daemon --config=/userdata/system/rclone.conf
 rclone mount eye:Games/eXo/eXoDOS_v6r2/eXo/eXoDOS/ "/userdata/roms/dos/MS-DOS eXoDOS" --http-no-head --no-checksum --no-modtime --dir-cache-time 1000h --allow-non-empty --attr-timeout 1000h --poll-interval 1000h --daemon --config=/userdata/system/rclone.conf
 
-archives=( "https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" )
-archives+=( https://wizzardsk.github.io/{tic80,wasm4,uzebox,lowresnx}.zip )
+archives=("https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" https://wizzardsk.github.io/{tic80,wasm4,uzebox,lowresnx}.zip)
 
 IFS=";"; for each in "${roms[@]}"; do 
   read -ra rom < <(printf '%s' "$each")
   if [ ! -f /userdata/thumb/${rom[0]}.png ]; then wget -nv -O /userdata/thumb/${rom[0]}.png https://raw.githubusercontent.com/fabricecaruso/es-theme-carbon/master/art/consoles/${rom[0]}.png; fi
-  rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}"); mkdir -p /userdata/roms/${rom[0]}/${rom3}
-  mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/${rom3}
+  rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}"); mkdir -p /userdata/roms/${rom[0]}/${rom3}; mount -o bind /userdata/rom/${rom[1]} /userdata/roms/${rom[0]}/${rom3}
 done
 
 /userdata/system/ratarmount -o attr_timeout=3600 --disable-union-mount "${archives[@]}" /userdata/zips -f & 
@@ -36,12 +34,9 @@ DAT_URL="https://github.com/WizzardSK/gameflix/raw/refs/heads/main/neogeo.dat"; 
 SRC_DIR="/userdata/rom/Internet Archive/chadmaster/fbnarcade-fullnonmerged/arcade"; DEST_DIR="/userdata/roms/neogeo/Neo Geo"
 curl -s -L "$DAT_URL" -o "$DAT_FILE"; mkdir -p "$DEST_DIR"
 while IFS= read -r line; do
-  neo_file="${line%%[[:space:]]*}"
-  [[ -z "$neo_file" || "$neo_file" != *.neo ]] && continue
-  base_name="${neo_file%.neo}"
-  zip_name="$base_name.zip"
-  src_file="$SRC_DIR/$zip_name"
-  dest_link="$DEST_DIR/$zip_name"
+  neo_file="${line%%[[:space:]]*}"; [[ -z "$neo_file" || "$neo_file" != *.neo ]] && continue
+  base_name="${neo_file%.neo}"; zip_name="$base_name.zip"
+  src_file="$SRC_DIR/$zip_name"; dest_link="$DEST_DIR/$zip_name"
   if [[ -f "$src_file" ]]; then ln -sf "$src_file" "$dest_link"; fi
 done < "$DAT_FILE"
 
