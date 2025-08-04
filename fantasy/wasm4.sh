5e5fe0fbd335f0da1f80e8c9118e1a88821b625a
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-04 21:26:56 +0200

    Update wasm4.sh

--- fantasy/wasm4.sh
+++ fantasy/wasm4.sh
@@ -2,12 +2,14 @@
 BASE_URL="https://wasm4.org/play"
 CARTS_URL="https://wasm4.org/carts"
 DIR="$GITHUB_WORKSPACE/wasm4"
-mkdir "$DIR"
+mkdir "$DIR" ~/backup
+unzip "$GITHUB_WORKSPACE/fantasy/wasm4.zip" ~/backup/
 curl -sL "$BASE_URL" | grep -oP '(?<=href="/play/)[^"]+' | sort -u | while read -r GAME; do
   for EXT in wasm png; do
     FILE="$DIR/$GAME.$EXT"; 
     [[ "$EXT" == "png" ]] && FILE="$DIR/$GAME.$EXT"; 
-    [[ -f "$FILE" ]] || wget -nv -O "$FILE" "$CARTS_URL/$GAME.$EXT"; 
+#    [[ -f "$FILE" ]] || wget -nv -O "$FILE" "$CARTS_URL/$GAME.$EXT"; 
+    [[ -f "$FILE" ]] || cp "backup/$FILE" "$FILE" || wget -nv -O "$FILE" "$CARTS_URL/$GAME.$EXT"
   done
 done
 cd "$GITHUB_WORKSPACE/wasm4"
