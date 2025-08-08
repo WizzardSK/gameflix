90cb7624b3d44b014ffe1d7b5b66c724667f7d39
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-08 15:38:21 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
-mkdir -p ~/myrient ~/roms/{Atari\ 2600\ ROMS,TIC-80,LowresNX,Uzebox,WASM-4,Vircon32,Lutro,MS-DOS\ eXoDOS,Socrates} ~/iso ~/zips ~/gameflix
+mkdir -p ~/myrient ~/roms/{Atari\ 2600\ ROMS,TIC-80,LowresNX,Uzebox,WASM-4,Vircon32,Lutro,MS-DOS\ eXoDOS,Socrates,TI99} ~/iso ~/zips ~/gameflix
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 archives=("https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" https://wizzardsk.github.io/{lowresnx,tic80,wasm4,uzebox,socrates}.zip)
 
@@ -8,6 +8,7 @@ if ! mountpoint -q "$HOME/myrient"; then rclone mount myrient: ~/myrient --http-
 if ! mountpoint -q "$HOME/roms/MS-DOS eXoDOS"; then rclone mount eye:Games/eXo/eXoDOS_v6r2/eXo/eXoDOS/ "$HOME/roms/MS-DOS eXoDOS" --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
 if ! mountpoint -q "$HOME/zips"; then nohup ratarmount -o attr_timeout=3600 --disable-union-mount "${archives[@]}" ~/zips -f & fi
 if ! mountpoint -q "$HOME/roms/Vircon32"; then rclone mount archive:all_vircon32_roms_and_media/all_vircon32_roms_and_media $HOME/roms/Vircon32 --daemon; fi
+if ! mountpoint -q "$HOME/roms/TI99"; then rclone mount whtech:MAME/rpk $HOME/roms/TI99 --daemon; fi
 while ! mountpoint -q "$HOME/zips"; do sleep 5; done
 
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/Atari-2600-VCS-ROM-Collection.zip/ROMS "$HOME/roms/Atari 2600 ROMS"
