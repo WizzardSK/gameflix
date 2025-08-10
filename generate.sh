1b059b4123cba88969008b01ea90e8f30946ce24
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-10 09:19:20 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -66,12 +66,12 @@ echo "<folder><path>./Voxatron</path><name>Voxatron</name><image>./splore.png</i
 
 IFS=";"; for each in "${roms[@]}"; do
   read -ra rom < <(printf '%s' "$each"); rom3=$(sed 's/<[^>]*>//g' <<< "${rom[3]}"); mkdir -p ~/mount/${rom[0]} ~/gamelists/${rom[0]}; romfolder="myrient/${rom[1]}"; emufolder="${rom[1]}";
-  if [[ "${rom[1]}" == *"eXoDOS"* ]];    then romfolder=emufolder="roms/MS-DOS eXoDOS"; fi
-  if [[ "${rom[1]}" == *"2600 ROMS"* ]]; then romfolder=emufolder="roms/Atari 2600 ROMS"; fi
-  if [[ "${rom[1]}" == *"Vircon32"* ]];  then romfolder=emufolder="roms/Vircon32"; fi
-  if [[ "${rom[1]}" == *"Lutro"* ]];     then romfolder=emufolder="roms/Lutro"; fi
-  if [[ "${rom[1]}" == *"Socrates"* ]];  then romfolder=emufolder="roms/Socrates"; fi
-  if [[ "${rom[1]}" == *"TI99"* ]];      then romfolder=emufolder="roms/TI99"; fi
+  if [[ "${rom[1]}" == *"eXoDOS"* ]];    then romfolder="roms/MS-DOS eXoDOS"; emufolder=$romfolder; fi
+  if [[ "${rom[1]}" == *"2600 ROMS"* ]]; then romfolder="roms/Atari 2600 ROMS"; emufolder=$romfolder; fi
+  if [[ "${rom[1]}" == *"Vircon32"* ]];  then romfolder="roms/Vircon32"; emufolder=$romfolder; fi
+  if [[ "${rom[1]}" == *"Lutro"* ]];     then romfolder="roms/Lutro"; emufolder=$romfolder; fi
+  if [[ "${rom[1]}" == *"Socrates"* ]];  then romfolder="roms/Socrates"; emufolder=$romfolder; fi
+  if [[ "${rom[1]}" == *"TI99"* ]];      then romfolder="roms/TI99"; emufolder=$romfolder; fi
   > ~/gameflix/${rom3}.html; echo ${rom3}; cp platform.html ~/gameflix/${rom3}.html
   echo "<script>bgImage(\"${rom[0]}\"); const fileNames = [" >> ~/gameflix/${rom3}.html; pocet=0; while IFS= read -r line; do
     line2="${line%.*}"; echo "\"${line}\"," >> ~/gameflix/${rom3}.html; ((pocet++)); ((total++))
