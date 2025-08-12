84bfee18e8390679bcfff9493460030d9ee783c8
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-08-12 17:52:07 +0200

    Update tic80.sh

--- fantasy/tic80.sh
+++ fantasy/tic80.sh
@@ -3,7 +3,7 @@ API_URL="https://tic80.com/api?fn=dir&path=play/Games"
 BASE_URL="https://tic80.com/cart"
 DOWNLOAD_DIR="$HOME/tic80"
 mkdir $DOWNLOAD_DIR ~/backup
-unzip "$GITHUB_WORKSPACE/fantasy/tic80.zip" -d ~/backup/
+unzip -q "$GITHUB_WORKSPACE/fantasy/tic80.zip" -d ~/backup/
 RESPONSE=$(curl -s "$API_URL")
 FILES=$(echo "$RESPONSE" | grep -oP '{\s*name\s*=\s*"[^"]+",\s*hash\s*=\s*"[^"]+",\s*id\s*=\s*\d+,\s*filename\s*=\s*"[^"]+"\s*}')
 echo "$FILES" | while read -r LINE; do
@@ -17,6 +17,6 @@ echo "$FILES" | while read -r LINE; do
 done
 cd $DOWNLOAD_DIR
 rm -f "$GITHUB_WORKSPACE/fantasy/tic80.zip" 
-zip -r "$GITHUB_WORKSPACE/fantasy/tic80.zip" *
+zip -q -r "$GITHUB_WORKSPACE/fantasy/tic80.zip" *
 cd "$GITHUB_WORKSPACE"
 git add "$GITHUB_WORKSPACE/fantasy/tic80.zip"
