598c15f4b271e1bbad6d86dc94ca0f55f1f472a0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-09-23 20:26:50 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
-mkdir -p ~/myrient ~/roms/{Atari\ 2600\ ROMS,TIC-80,LowresNX,Uzebox,WASM-4,Vircon32,MS-DOS\ eXoDOS,Socrates,TI99} ~/iso ~/zips ~/gameflix
+mkdir -p ~/myrient ~/roms/{Atari\ 2600\ ROMS,TIC-80,LowresNX,Uzebox,WASM-4,Vircon32,MS-DOS\ eXoDOS,TDC,Socrates,TI99} ~/iso ~/zips ~/gameflix
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 archives=("https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip" https://wizzardsk.github.io/{lowresnx,tic80,wasm4,uzebox,socrates}.zip)
 
@@ -9,6 +9,7 @@ if ! mountpoint -q "$HOME/roms/MS-DOS eXoDOS"; then rclone mount eye:Games/eXo/e
 if ! mountpoint -q "$HOME/zips"; then nohup ratarmount -o attr_timeout=3600 --disable-union-mount "${archives[@]}" ~/zips -f & fi
 if ! mountpoint -q "$HOME/roms/Vircon32"; then rclone mount archive:all_vircon32_roms_and_media/all_vircon32_roms_and_media $HOME/roms/Vircon32 --daemon; fi
 if ! mountpoint -q "$HOME/roms/TI99"; then rclone mount whtech:MAME/rpk $HOME/roms/TI99 --daemon; fi
+if ! mountpoint -q "$HOME/roms/TDC"; then rclone mount "turran:Non Amiga/DOS/Total.DOS.Collection.v22/Games/Files" $HOME/roms/TDC --daemon; fi
 while ! mountpoint -q "$HOME/zips"; do sleep 5; done
 
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/Atari-2600-VCS-ROM-Collection.zip/ROMS "$HOME/roms/Atari 2600 ROMS"
@@ -20,8 +21,7 @@ bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/so
 
 DAT_URL="https://github.com/WizzardSK/gameflix/raw/refs/heads/main/neogeo.dat"; DAT_FILE="/tmp/neogeo.dat"
 SRC_DIR="$HOME/myrient/Internet Archive/chadmaster/fbnarcade-fullnonmerged/arcade"; DEST_DIR="$HOME/roms/Neo Geo"
-curl -s -L "$DAT_URL" -o "$DAT_FILE"; mkdir -p "$DEST_DIR"
-while IFS= read -r line; do
+curl -s -L "$DAT_URL" -o "$DAT_FILE"; mkdir -p "$DEST_DIR"; while IFS= read -r line; do
   neo_file="${line%%[[:space:]]*}"; [[ -z "$neo_file" || "$neo_file" != *.neo ]] && continue
   base_name="${neo_file%.neo}"; zip_name="$base_name.zip"; src_file="$SRC_DIR/$zip_name"; dest_link="$DEST_DIR/$zip_name"
   if [[ -f "$src_file" ]]; then ln -sf "$src_file" "$dest_link"; fi
