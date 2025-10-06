09dc93b23ecf4fbc22487a6e4bf73f0174e6f0de
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-10-06 21:04:39 +0200

    Update webflix.sh

--- webflix.sh
+++ webflix.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
-mkdir -p ~/myrient ~/roms/{TIC-80,LowresNX,WASM-4,TI99} ~/iso ~/zips ~/gameflix
+mkdir -p ~/myrient ~/roms/{TIC-80,LowresNX,WASM-4} ~/iso ~/zips ~/gameflix
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 archives=(https://wizzardsk.github.io/{lowresnx,tic80,wasm4}.zip)
 
 if ! mountpoint -q "$HOME/myrient"; then rclone mount myrient: ~/myrient --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
 if ! mountpoint -q "$HOME/zips"; then nohup ratarmount-full -o attr_timeout=3600 --disable-union-mount "${archives[@]}" ~/zips -f & fi
-if ! mountpoint -q "$HOME/roms/TI99"; then rclone mount whtech:MAME/rpk $HOME/roms/TI99 --daemon; fi
 while ! mountpoint -q "$HOME/zips"; do sleep 5; done
 
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/lowresnx.zip "$HOME/roms/LowresNX"
