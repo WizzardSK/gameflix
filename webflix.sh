cff7b10a180c90ab24d24460a131d188d3019d96
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-10-19 19:48:51 +0200

    Remove DICE directory and related bindings

--- webflix.sh
+++ webflix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
-mkdir -p ~/myrient ~/roms/{TIC-80,LowresNX,WASM-4,DICE} ~/iso ~/zips ~/gameflix
+mkdir -p ~/myrient ~/roms/{TIC-80,LowresNX,WASM-4} ~/iso ~/zips ~/gameflix
 wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
-archives=(https://wizzardsk.github.io/{lowresnx,tic80,wasm4,dice}.zip)
+archives=(https://wizzardsk.github.io/{lowresnx,tic80,wasm4}.zip)
 
 if ! mountpoint -q "$HOME/myrient"; then rclone mount myrient: ~/myrient --http-no-head --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --daemon --no-check-certificate --allow-other; fi
 if ! mountpoint -q "$HOME/zips"; then nohup ratarmount-full -o attr_timeout=3600 --disable-union-mount "${archives[@]}" ~/zips -f & fi
@@ -11,4 +11,3 @@ while ! mountpoint -q "$HOME/zips"; do sleep 5; done
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/lowresnx.zip "$HOME/roms/LowresNX"
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/tic80.zip "$HOME/roms/TIC-80"
 bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/wasm4.zip "$HOME/roms/WASM-4"
-bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) $HOME/zips/dice.zip "$HOME/roms/DICE"
