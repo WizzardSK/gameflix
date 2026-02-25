d9f9a590fba498a51ea76f1bf8fbc770817e8b29
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-02-25 21:04:08 +0100

    Delete script2.js

--- script2.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('img');
-    images.forEach(image => {
-        image.addEventListener('load', function() { image.classList.add('loaded'); });
-        if (image.complete) { image.classList.add('loaded'); }
-    });
-});
