e6df23838f2768e096c2eb9728ec5a1921351b36
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-04-16 21:19:09 +0200

    Add zip filenames to tosec-main paths and fix rclone mount

--- webflix.sh
+++ webflix.sh
@@ -15,21 +15,24 @@ csv=$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platform
 mounted=()
 while IFS=';' read -ra rom; do
   path="${rom[1]}"
-  [[ "$path" =~ ^archive:([^/]+)(.*) ]] && archive="${BASH_REMATCH[1]}" subpath="${BASH_REMATCH[2]}"
-  [[ -n "$archive" ]] || continue
+  [[ "$path" =~ ^archive:([^/]+)/(.+) ]] && remote="${BASH_REMATCH[1]}" subpath="/${BASH_REMATCH[2]}"
+  [[ -n "$remote" ]] || continue
+  archive_path=$(dirname "$subpath")
   already_mounted=false
-  for m in "${mounted[@]}"; do [[ "$m" == "$archive" ]] && already_mounted=true && break; done
+  for m in "${mounted[@]}"; do [[ "$m" == "$remote" ]] && already_mounted=true && break; done
   if [[ "$already_mounted" == "false" ]]; then
-    mounted+=("$archive")
-    mkdir -p ~/zips/$archive
-    mountpoint -q ~/zips/$archive || rclone mount "$archive:" ~/zips/$archive --daemon --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --poll-interval 1000h --allow-non-empty --allow-other --vfs-cache-mode minimal --vfs-read-chunk-size 1M
+    mounted+=("$remote")
+    mkdir -p ~/zips/$remote
+    mountpoint -q ~/zips/$remote || nohup rclone mount "archive:$remote" ~/zips/$remote --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --allow-non-empty --allow-other --vfs-cache-mode minimal --vfs-read-chunk-size 1M > /dev/null 2>&1 &
+    while ! mountpoint -q ~/zips/$remote; do sleep 5; done
+    sleep 2
   fi
 done <<< "$csv"
 while IFS=';' read -ra rom; do
   platform="${rom[0]}" path="${rom[1]}" display="${rom[2]}"
-  [[ "$path" =~ ^archive:([^/]+)(.*) ]] && archive="${BASH_REMATCH[1]}" subpath="${BASH_REMATCH[2]}"
-  [[ -n "$archive" ]] || continue
+  [[ "$path" =~ ^archive:([^/]+)/(.+) ]] && remote="${BASH_REMATCH[1]}" subpath="/${BASH_REMATCH[2]}"
+  [[ -n "$remote" ]] || continue
   display=$(sed 's/<[^>]*>//g' <<< "$display")
   mkdir -p ~/roms/$platform/$display
-  bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/zips/$archive$subpath ~/roms/$platform/$display
+  bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/zips/$remote$subpath ~/roms/$platform/$display
 done <<< "$csv"
