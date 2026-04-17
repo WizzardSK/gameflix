fd79253ec3d47e86f4954d8a63d58a0d62998b9e
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-04-17 19:58:44 +0200

    fix mame-sl path

--- webflix.sh
+++ webflix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
 mkdir -p ~/roms/{LowresNX,WASM-4} ~/rom/{mame-sl,ni-roms,tosec-main,tosec-iso-ibm,tosec-iso-namco-sega-nintendo,tosec-iso-nec,tosec-iso-philips,tosec-iso-sega,tosec-iso-sony,tosec-iso-3do,tosec-iso-commodore,exov5_2,all_vircon32_roms_and_media,a2600_romhunter,mame-software-list-chds-2,MAME0.106-Reference-Set-ROMs-CHDs-Samples,MAME0.37b5_MAME2000_Reference_Set_Update_2_ROMs_Samples,MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples,MAME_2010_full_nonmerged_romsets,MAME_2015_arcade_romsets,MAME_2016_Arcade_Romsets,m2emu1.1a,SegaMD-Enhanced-ROMs,segamodel3} ~/iso ~/zips ~/gameflix
-wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
+wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizardSK/gameflix/main/rclone.conf
 
 if [ ! -f $HOME/ratarmount-full ]; then wget -nv -O $HOME/ratarmount-full https://github.com/mxmlnkn/ratarmount/releases/download/v1.2.0/ratarmount-1.2.0-x86_64.AppImage; chmod +x $HOME/ratarmount-full; fi
 
-csv=$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.csv | tail -n +2)
+csv=$(curl -s https://raw.githubusercontent.com/WizardSK/gameflix/main/platforms.csv | tail -n +2)
 
 remotes_done=()
 sources=("https://wizzardsk.github.io/lowresnx.zip" "https://wizzardsk.github.io/wasm4.zip")
@@ -24,7 +24,11 @@ while IFS=',' read -ra rom; do
   fi
   
   mkdir -p ~/roms/$platform/"$display"
-  bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/rom/$remote/roms ~/roms/$platform/"$display"
+  if [[ "$remote" == "mame-sl" ]]; then
+    bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/rom/$remote ~/roms/$platform/"$display"
+  else
+    bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/rom/$remote/roms ~/roms/$platform/"$display"
+  fi
 done <<< "$csv"
 
 nohup $HOME/ratarmount-full -o attr_timeout=3600 --disable-union-mount "${sources[@]}" ~/zips -f &
