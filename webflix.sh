167d3213e3e0d801752609f288b952a4a0737bb0
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-04-17 20:17:51 +0200

    use bindfs directly on rclone mounts

--- webflix.sh
+++ webflix.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
 mkdir -p ~/roms/{LowresNX,WASM-4} ~/rom/{mame-sl,ni-roms,tosec-main,tosec-iso-ibm,tosec-iso-namco-sega-nintendo,tosec-iso-nec,tosec-iso-philips,tosec-iso-sega,tosec-iso-sony,tosec-iso-3do,tosec-iso-commodore,exov5_2,all_vircon32_roms_and_media,a2600_romhunter,mame-software-list-chds-2,MAME0.106-Reference-Set-ROMs-CHDs-Samples,MAME0.37b5_MAME2000_Reference_Set_Update_2_ROMs_Samples,MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples,MAME_2010_full_nonmerged_romsets,MAME_2015_arcade_romsets,MAME_2016_Arcade_Romsets,m2emu1.1a,SegaMD-Enhanced-ROMs,segamodel3} ~/iso ~/zips ~/gameflix
-wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizardSK/gameflix/main/rclone.conf
+wget -nv -O ~/.config/rclone/rclone.conf https://raw.githubusercontent.com/WizzardSK/gameflix/main/rclone.conf
 
 if [ ! -f $HOME/ratarmount-full ]; then wget -nv -O $HOME/ratarmount-full https://github.com/mxmlnkn/ratarmount/releases/download/v1.2.0/ratarmount-1.2.0-x86_64.AppImage; chmod +x $HOME/ratarmount-full; fi
 
-csv=$(curl -s https://raw.githubusercontent.com/WizardSK/gameflix/main/platforms.csv | tail -n +2)
+csv=$(curl -s https://raw.githubusercontent.com/WizzardSK/gameflix/main/platforms.csv | tail -n +2)
 
 remotes_done=()
-sources=("https://wizzardsk.github.io/lowresnx.zip" "https://wizzardsk.github.io/wasm4.zip")
+
+nohup $HOME/ratarmount-full -o attr_timeout=3600 --disable-union-mount https://wizzardsk.github.io/lowresnx.zip https://wizzardsk.github.io/wasm4.zip ~/zips -f &
+while ! mountpoint -q ~/zips; do sleep 5; done
+
+bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/zips/lowresnx ~/roms/LowresNX
+bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/zips/wasm4 ~/roms/WASM-4
 
 while IFS=',' read -ra rom; do
   platform="${rom[0]}" path="${rom[1]}" display="${rom[2]}"
@@ -20,7 +25,6 @@ while IFS=',' read -ra rom; do
     remotes_done+=("$remote")
     nohup rclone mount "archive:$remote" ~/rom/$remote --no-checksum --no-modtime --attr-timeout 1000h --dir-cache-time 1000h --allow-non-empty --allow-other --vfs-cache-mode minimal --vfs-read-chunk-size 1M > /dev/null 2>&1 &
     while ! mountpoint -q ~/rom/$remote; do sleep 5; done
-    sources+=("~/rom/$remote")
   fi
   
   mkdir -p ~/roms/$platform/"$display"
@@ -30,6 +34,3 @@ while IFS=',' read -ra rom; do
     bindfs --perms=0755 --force-user=$(whoami) --force-group=$(id -gn) ~/rom/$remote/roms ~/roms/$platform/"$display"
   fi
 done <<< "$csv"
-
-nohup $HOME/ratarmount-full -o attr_timeout=3600 --disable-union-mount "${sources[@]}" ~/zips -f &
-while ! mountpoint -q ~/zips; do sleep 5; done
