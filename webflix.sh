2ce3aa5e49ba0f8afd38fc758a62e6fd1aff5499
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-04-19 18:28:49 +0200

    fix: use copyfile to prevent nested zip dirs

--- webflix.sh
+++ webflix.sh
@@ -28,7 +28,7 @@ while read path; do
   
   mkdir -p "$(dirname "$target")"
   [[ -f "$target" ]] && continue
-  rclone copy "$path" "$target" --no-check-dest 2>/dev/null &
+  rclone copyfile "$path" "$target" --no-check-dest 2>/dev/null &
   
   while (( $(jobs -r | wc -l) >= 10 )); do sleep 2; done
 done <<< "$csv"
