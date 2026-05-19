6c21b34695c0e97231784ba0a843cb99e0d951bc
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-05-19 21:36:04 +0200

    play handler: drop hardcoded $HOME path from generated URIs
    platform.js now emits play:///<platform>/<folder>/<file> (relative under
    gameflix's roms dir) instead of baking /home/wizzard/share/roms/ into
    every link. retroarch.sh resolves the stripped path against
    $HOME/share/roms when it doesn't already exist absolute, so old
    generated HTML keeps working.

--- platform.js
+++ platform.js
@@ -103,7 +103,7 @@ function generateFileLinks(romPath, imagePath) {
         var platform = location.pathname.split('/').pop().replace(/\.html?$/, '');
         var headers = document.querySelectorAll('.section-header');
         var foldername = headers.length ? headers[headers.length - 1].id : '';
-        romPath = 'play:///home/wizzard/share/roms/' + platform + '/' + foldername;
+        romPath = 'play:///' + platform + '/' + foldername;
     }
     var encodedPath = encodeURI(romPath);
     var html = [];
--- retroarch.sh
+++ retroarch.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Strip play:// URL scheme + URL-decode
+# Strip play:// URL scheme + URL-decode. New gameflix HTML emits a relative
+# path like /atari2600/NoIntro/foo.zip which we resolve under ~/share/roms;
+# old HTML with the full absolute path is detected via existence and used
+# as-is.
 arg="$1"
 [[ "$arg" == play://* ]] && arg="${arg#play://}"
 arg=$(printf '%b' "${arg//%/\\x}")
+[[ -e "$arg" ]] || arg="$HOME/share/roms$arg"
 set -- "$arg" "${@:2}"
 head "$1"
 adresar=$(dirname "$1")
